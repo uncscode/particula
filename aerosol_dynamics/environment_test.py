5bda65199bb015f71f889e2bf62f783576ab490f
ngam <67342040+ngam@users.noreply.github.com>
2021-12-18 11:23:51 -0500

    making linter happy (docstrings)

--- aerosol_dynamics/environment_test.py
+++ aerosol_dynamics/environment_test.py
@@ -1,6 +1,13 @@
-from aerosol_dynamics import environment
+"""
+testing new env class
+"""
+
 import pytest
+
+from aerosol_dynamics import environment
+
 from . import u
+
 
 standard_environment = environment.Environment(
     temperature=298 * u.K,
@@ -8,15 +15,21 @@ standard_environment = environment.Environment(
 )
 
 def test_getters():
+    """testing getters
+    """
     assert standard_environment.temperature() == 298 * u.K
     assert standard_environment.pressure() == 101325 * u.Pa
 
 def test_dynamic_viscosity_air():
+    """testing dynamic visc
+    """
     assert (standard_environment.dynamic_viscosity_air().magnitude ==
         pytest.approx(1.836e-5, rel=1e-3)
         )
 
 def test_mean_free_path_air():
+    """testing mfp
+    """
     assert (standard_environment.mean_free_path_air().magnitude ==
         pytest.approx(6.644e-8, rel=1e-3)
         )
