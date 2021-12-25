e1c19e15313f91b7a6e3bd8d5961ff176c117e6f
ngam <67342040+ngam@users.noreply.github.com>
2021-12-24 19:27:26 -0500

    adding init description of particle

--- particula/aerosol_dynamics/__init__.py
+++ particula/aerosol_dynamics/__init__.py
@@ -1,5 +1,9 @@
-""".
+"""module to perform calculations related to aerosol dynamics.
 
-It can do the following:
+This module contains functions to perform calculations related to
+aerosol dynamics. It inherits the unit registry, u, from particula.
+The pacakge contents below are callablle. For example, the particle
+module is callable as: from particula.aerosol_dynamics import particle.
 
+More details to follow.
 """
