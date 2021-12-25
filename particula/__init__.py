1890fc9422409acf36550650c889a90b3351afa2
ngam <67342040+ngam@users.noreply.github.com>
2021-12-24 19:20:40 -0500

    add particula description in __init__

--- particula/__init__.py
+++ particula/__init__.py
@@ -1,5 +1,11 @@
-"""
-PARTICULA!
+"""a simple, fast, and powerful particle simulator.
+
+particula is a simple, fast, and powerful particle simulator, or at
+least two of the three, we hope. It is a simple particle system that
+is designed to be easy to use and easy to extend. The goal is to have
+a robust aerosol (gas phase and particle phase) simulation system that
+can be used to answer scientific questions that arise for experiments
+and research discussions.
 """
 
 __version__ = '0.0.2'
