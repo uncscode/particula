e0580fcee66aaba9aa54a95263a2a8e3749df1c9
ngam <67342040+ngam@users.noreply.github.com>
2022-02-07 16:28:55 -0500

    clean up env class

--- particula/aerosol_dynamics/environment.py
+++ particula/aerosol_dynamics/environment.py
@@ -1,69 +1,58 @@
-"""environment class
+""" defining the environment class.
+
+    The environment is defined by:
+        - the temperature of the environment in K
+        - the pressure inside the environment in Pa
+
+    The enviroment class is used to derivate properties:
+        - the mean free path in air
+        - the dynamic viscosity in air
 """
 
-import numpy as np
 from particula import u
-from particula.aerosol_dynamics import physical_parameters as pp
+from particula.utils.environment_ import(
+    mean_free_path_air as mfp_air,
+    dynamic_viscosity_air as dyn_vis_air,
+)
 
 
 class Environment:
-    """
-    Sets the environment class
-    with properties such as temperature and pressure
-    and derived properties such as air viscosity.
+    """ sets the environment class.
     """
     def __init__(self, temperature, pressure):
-        """Function calls for enviornment class."""
+        """ initiates the environment class.
+
+            Inputs:
+                - temperature
+                - pressure
+        """
         self._temperature = temperature
         self._pressure = pressure
 
     @u.wraps(u.K, [None])
     def temperature(self) -> float:
-        """Returns the temperature of the environment."""
+        """ Returns the temperature: [K]
+        """
+
         return self._temperature
 
     @u.wraps(u.Pa, [None])
     def pressure(self) -> float:
-        """Returns the pressure of the environment."""
+        """ Returns the pressure: [Pa]
+        """
+
         return self._pressure
 
     @u.wraps(u.kg / u.m / u.s, [None])
     def dynamic_viscosity_air(self) -> float:
-        """Returns the dynamic viscosity of air. [kg/m/s]
-
-        The dynamic viscosity is calculated using the 3 parameter
-        Sutherland Viscosity Law.
+        """ Returns the dynamic viscosity of air: [kg/m/s]
         """
-        mu_ref = 1.716e-5 * u.Pa * u.s  # Viscosity at T_REF
-        t_ref = 273.15 * u.K
-        suth_const = 110.4 * u.K  # Sutherland constant
 
-        return (
-            mu_ref *
-            (self.temperature()/t_ref)**(3/2) *
-            (t_ref + suth_const) / (self.temperature() + suth_const)
-        )
+        return dyn_vis_air(self.temperature())
 
-    # mean free path of air in m
     @u.wraps(u.m, [None])
     def mean_free_path_air(self) -> float:
-        """Returns the mean free path of this environment. [m]
-
-        The mean free path is the average distance traveled by a molecule
-        between collisions with other molecules.
+        """ Returns the mean free path in air: [m]
         """
-        # Molecular weight of air in kg/mol
-        molecular_weight = (28.9644 * u.g / u.mol).to_base_units()
 
-        return (
-            (
-                2 * self.dynamic_viscosity_air()
-                / self.pressure()
-                / (
-                    8*molecular_weight
-                    / (
-                        np.pi*pp.GAS_CONSTANT*self.temperature()
-                    )
-                )**0.5
-            ).to_base_units()
-        )
+        return mfp_air(self.temperature(), self.pressure())
--- particula/aerosol_dynamics/tests/environment_test.py
+++ particula/aerosol_dynamics/tests/environment_test.py
@@ -1,4 +1,8 @@
-"""tests for the environment module.
+""" tests for the Environment class.
+
+    1. Test getters for the Environment class.
+    2. Test calculation for the dynamic viscosity of air.
+    3. Test calculation for the mean free path of air.
 """
 
 import pytest
@@ -12,17 +16,17 @@ standard_environment = environment.Environment(
 
 
 def test_getters():
+    """ tests the getters for the Environment class.
     """
-    Tests the getters for the Environment class.
-    """
+
     assert standard_environment.temperature() == 298 * u.K
     assert standard_environment.pressure() == 101325 * u.Pa
 
 
 def test_dynamic_viscosity_air():
+    """ tests calculation for the dynamic viscosity of air.
     """
-    Tests that the calculation for the dynamic viscosity of air works.
-    """
+
     assert (
         standard_environment.dynamic_viscosity_air().magnitude ==
         pytest.approx(1.716e-5, rel=1e-1)
@@ -30,9 +34,9 @@ def test_dynamic_viscosity_air():
 
 
 def test_mean_free_path_air():
+    """ test calculation for the mean free path of air.
     """
-    Test that the calculation for the mean free path of air works.
-    """
+
     assert (
         standard_environment.mean_free_path_air().magnitude ==
         pytest.approx(6.644e-8, rel=1e-1)
