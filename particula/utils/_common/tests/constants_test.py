8d2438fd7f3cda6c8e7cc6b872aafc8795a16212
ngam <67342040+ngam@users.noreply.github.com>
2022-02-09 12:15:54 -0500

    set up constants

---
+++ particula/utils/_common/tests/constants_test.py
@@ -0,0 +1,42 @@
+""" Testing getting constants from the get_constants.py file
+"""
+
+from particula import u
+from particula.utils._common.get_constants import (
+    BOLTZMANN_CONSTANT,
+    AVOGADRO_NUMBER,
+    GAS_CONSTANT,
+)
+
+
+def test_get_constants():
+
+    """ simple tests are conducted as follows:
+
+            * see if GAS_CONSTANT maniuplation is good
+            * see if BOLTZMANN_CONSTANT units are good
+    """
+
+    assert (
+        GAS_CONSTANT
+        ==
+        BOLTZMANN_CONSTANT * AVOGADRO_NUMBER
+    )
+
+    assert (
+        GAS_CONSTANT.units
+        ==
+        BOLTZMANN_CONSTANT.units * AVOGADRO_NUMBER.units
+    )
+
+    assert (
+        GAS_CONSTANT.magnitude
+        ==
+        BOLTZMANN_CONSTANT.magnitude * AVOGADRO_NUMBER.magnitude
+    )
+
+    assert (
+        BOLTZMANN_CONSTANT.units
+        ==
+        u.m**2 * u.kg / u.s**2 / u.K
+    )
