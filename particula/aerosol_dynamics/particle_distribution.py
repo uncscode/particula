5099b90247024ff323d8a57dc902fa0244275732
Kyle Gorkowski <kkgorkow@gmail.com>
2022-02-18 16:01:42 -0700

    updated rasterization

--- particula/aerosol_dynamics/particle_distribution.py
+++ particula/aerosol_dynamics/particle_distribution.py
@@ -31,9 +31,8 @@ import numpy as np
 class Particle_distribution:
     """Class to instantiate particle distributions.
 
-    This class represents the underlying framework for both
-    particle--particle and gas--particle interactions. See detailed
-    methods and functions below.
+    This class represents the underlying framework for
+    storing particle distribution data
 
     Attributes:
 
@@ -68,21 +67,20 @@ class Particle_distribution:
         self._radii = radii
         self._density = density
         self._charge = charge
-        self._mass = density * (4*np.pi/3) * (radii**3)
         self._number = number
+        self._mass = density * (4*np.pi/3) * (radii**3)
+
 
     def name(self) -> str:
-        """Returns the name of particle.
+        """Returns the name of the distribution.
         """
-
         return self._name
 
     def masses(self) -> float:
-        """Returns mass of particles of that size.
+        """Returns total mass of particles of that radii.
 
         units: [kg]
         """
-
         return self._mass * self._number
 
     def radii(self) -> float:
@@ -90,7 +88,6 @@ class Particle_distribution:
 
         units: [m]
         """
-
         return self._radii
 
     def densities(self) -> int:
@@ -98,35 +95,200 @@ class Particle_distribution:
 
         units: [kg/m**3]
         """
-
         return self._density
 
     def charges(self) -> int:
-        """Returns number of charges on particle.
+        """Returns number of charges on each particle.
 
         units: [dimensionless]
         """
-
         return self._charge
     
     def number(self) -> int:
-        """Returns number of charges on particle.
+        """Returns number of particles.
 
         units: [dimensionless]
         """
-
         return self._number
 
     def number_concentration(self) -> int:
         """" Returns the number of distribution of particles.
+        
         units: [#/m**3]
         """
-
         return np.sum(self._number)
 
     def mass_concentration(self) -> int:
-        """" Returns the number of distribution of particles.
+        """" Returns the mass of the distribution.
+        
         units: [kg/m**3]
         """
-
         return np.sum(self.masses())
+
+    def rasterization(self, bins):
+        """ Returns binned radii and number using numpy histogram methods
+
+        Parameters:
+
+        bins : int or sequence of scalars or str, optional
+            If `bins` is an int, it defines the number of equal-width
+            bins in the given range (10, by default). If `bins` is a
+            sequence, it defines the bin edges, including the rightmost
+            edge, allowing for non-uniform bin widths.
+
+            If `bins` is a string from the list below, `histogram_bin_edges`
+            will use the method chosen to calculate the optimal bin width and
+            consequently the number of bins (see `Notes` for more detail on
+            the estimators) from the data that falls within the requested
+            range. While the bin width will be optimal for the actual data
+            in the range, the number of bins will be computed to fill the
+            entire range, including the empty portions. For visualisation,
+            using the 'auto' option is suggested.
+
+            'auto'
+                Maximum of the 'sturges' and 'fd' estimators. Provides good
+                all around performance.
+
+            'fd' (Freedman Diaconis Estimator)
+                Robust (resilient to outliers) estimator that takes into
+                account data variability and data size.
+
+            'doane'
+                An improved version of Sturges' estimator that works better
+                with non-normal datasets.
+
+            'scott'
+                Less robust estimator that that takes into account data
+                variability and data size.
+
+            'stone'
+                Estimator based on leave-one-out cross-validation estimate of
+                the integrated squared error. Can be regarded as a generalization
+                of Scott's rule.
+
+            'rice'
+                Estimator does not take variability into account, only data
+                size. Commonly overestimates number of bins required.
+
+            'sturges'
+                R's default method, only accounts for data size. Only
+                optimal for gaussian data and underestimates number of bins
+                for large non-gaussian datasets.
+
+            'sqrt'
+                Square root (of data size) estimator, used by Excel and
+                other programs for its speed and simplicity.
+
+        returns: 
+            particle_number     array of dtype float
+            radii               array of dtype float
+        """
+
+        # histogram method to rasterize the radii and 
+        # tracking the number concentration
+        bin_edges = np.histogram_bin_edges(self.radii(), bins=bins)
+        particle_number, bin_edges = np.histogram(
+            self.radii(),
+            bins=bin_edges,
+            weights = self.number()
+        )
+
+        # calculates bin centers and applies it as the radii
+        radii = np.diff(bin_edges)+bin_edges[0:-1] 
+
+        # drop the zero concentration bins, in radii and number
+        non_zeros = particle_number>0
+        particle_number = particle_number[non_zeros]
+        radii = radii[non_zeros]
+
+        return particle_number, radii
+
+    def update_distribution(self, rasterized_radii, rasterized_number):
+        """" updates the particle distribution properties, based on a 
+        new rasterization. 
+        
+        Conserves total particle mass.
+        Does not conserve number.
+        Assumes same density and charge.
+
+        This could be built out to have different options.
+
+        Parameters:
+
+            rasterized_radii:       array of dtype float
+            rasterized_number:      array of dtype float
+        """
+        old_mass = self.mass_concentration()
+
+        self._radii = rasterized_radii
+        self._density = np.ones(len(rasterized_radii))*self._density[0]
+        self._charge = np.ones(len(rasterized_radii))*self._density[0]
+        self._number = rasterized_number
+        self._mass = self._density * (4*np.pi/3) * (self._radii**3)
+
+        # adjustment to number so mass is conserved
+        new_mass = self.mass_concentration()
+        self._number = rasterized_number * old_mass/new_mass
+
+        return
+    
+    def rasterization_and_update(self, bins):
+        """ Bins the radii and number using numpy histogram methods, and updates
+        the class data
+
+        Parameters:
+
+        bins : int or sequence of scalars or str, optional
+            If `bins` is an int, it defines the number of equal-width
+            bins in the given range (10, by default). If `bins` is a
+            sequence, it defines the bin edges, including the rightmost
+            edge, allowing for non-uniform bin widths.
+
+            If `bins` is a string from the list below, `histogram_bin_edges`
+            will use the method chosen to calculate the optimal bin width and
+            consequently the number of bins (see `Notes` for more detail on
+            the estimators) from the data that falls within the requested
+            range. While the bin width will be optimal for the actual data
+            in the range, the number of bins will be computed to fill the
+            entire range, including the empty portions. For visualisation,
+            using the 'auto' option is suggested.
+
+            'auto'
+                Maximum of the 'sturges' and 'fd' estimators. Provides good
+                all around performance.
+
+            'fd' (Freedman Diaconis Estimator)
+                Robust (resilient to outliers) estimator that takes into
+                account data variability and data size.
+
+            'doane'
+                An improved version of Sturges' estimator that works better
+                with non-normal datasets.
+
+            'scott'
+                Less robust estimator that that takes into account data
+                variability and data size.
+
+            'stone'
+                Estimator based on leave-one-out cross-validation estimate of
+                the integrated squared error. Can be regarded as a generalization
+                of Scott's rule.
+
+            'rice'
+                Estimator does not take variability into account, only data
+                size. Commonly overestimates number of bins required.
+
+            'sturges'
+                R's default method, only accounts for data size. Only
+                optimal for gaussian data and underestimates number of bins
+                for large non-gaussian datasets.
+
+            'sqrt'
+                Square root (of data size) estimator, used by Excel and
+                other programs for its speed and simplicity.
+        """
+
+        particle_number, radii = self.rasterization(bins)
+        self.update_distribution(radii, particle_number)
+
+        return
--- particula/aerosol_dynamics/temp_for_particle_dist.py
+++ particula/aerosol_dynamics/temp_for_particle_dist.py
@@ -5,7 +5,6 @@ import os
 os.chdir('..')
 
 #%%
-# import particula.utils.strip_units as strip_units
 from particula.aerosol_dynamics import environment, particle_distribution, parcel
 from particula.utils.strip_units import make_unitless
 import numpy as np
@@ -23,19 +22,48 @@ standard_environment = environment.Environment(
 
 dist_sample = np.random.poisson(100, size=10000)*10**-9 # m
 
-bins_centers = np.linspace(0, 1000, 100)
+bin_edges = np.linspace(0, 1000, 100)*10**-9 # m
 
-particle_distribution(dist_sample, bins_centers)
+particle_dist = particle_distribution.Particle_distribution( radii = dist_sample,
+        density = np.ones(len(dist_sample))*1000,
+        charge = np.zeros(len(dist_sample)),
+        number = np.ones(len(dist_sample)),
+    )
+print(particle_dist.number_concentration())
+
+# def rasterization(particle_distribution, bins):
+#     """ 
+    
+#     """
 
 
-def distribution_rasterization(particle_distribution, bins_centers):
-    np.histogram(a, bins=10, range=None, normed=None, weights=None, density=None)
+#     # histogram method
+#     bin_edges = np.histogram_bin_edges(particle_distribution.radii(), bins=bins)
+#     particle_number, bin_edges = np.histogram(
+#         particle_distribution.radii(),
+#         bins=bin_edges,
+#         weights = particle_distribution.number()
+#     )
+
+#     bin_centers = np.diff(bin_edges)+bin_edges[0:-1] # calculates bin centers and applies as the radii
+
+#     # drop zero bins
+#     non_zeros = particle_number>0
+#     particle_number = particle_number[non_zeros]
+#     bin_centers = bin_centers[non_zeros]
+
+#     return particle_number, bin_centers
+
+# number_raster, bin_centers = distribution_rasterization(particle_dist, 'auto')
 
 
+# particle_dist.update_distribution(bin_centers, number_raster)
+# print(particle_dist.number_concentration())
 
-np.logspace(10**-9, 10**-1, num=1000)
-charges_array = np.array([0, 0, 0, 0])
-charge_other = charges_array[0]
+
+# np.logspace(10**-9, 10**-1, num=1000)
+# charges_array = np.array([0, 0, 0, 0])
+# charge_other = charges_array[0]
 #%%
 
 # %%
@@ -49,7 +77,7 @@ matplotlib.rcParams['font.size'] = '14'
 
 fig, ax= matplotlib.pyplot.subplots(1,1,figsize=[6,6])
 
-ax.hist(dist_sample, bins=50, density=True)
+ax.plot(bin_centers, number_raster)
 
 
 # ax.set_ylim(0,14000)
