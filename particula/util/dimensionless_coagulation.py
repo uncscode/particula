06483d5e94e19e559ba666fc3a0112bf78a430ad
ngam <67342040+ngam@users.noreply.github.com>
2022-02-19 17:15:56 -0500

    add dimless coag

--- particula/util/dimensionless_coagulation.py
+++ particula/util/dimensionless_coagulation.py
@@ -3,77 +3,57 @@
 
 import numpy as np
 
-# from particula import u
-from particula.util import (
-    diffusive_knudsen,
-)
-
 
 class DimensionlessCoagulation:
 
     """ dimensionless coagulation
     """
 
-    def __init__(
-        self,
-        radius, other_radius,
-        density=1000, other_density=1000,
-        charge=0, other_charge=0,
-        temperature=298,
-        mfp_air=66.4e-9,
-        dyn_vis_air=1.8e-05,
-    ) -> float:
-
+    def __init__(self, **kwargs):
         """ Dimensionless particle--particle coagulation kernel.
 
             Attributes:
-                radius        (float) [m]
-                other_radius  (float) [m]
-                density       (float) [kg/m^3]        (default: 1000)
-                other_density (float) [kg/m^3]        (default: 1000)
-                charge        (int)   [dimensionless] (default: 0)
-                other_charge  (int)   [dimensionless] (default: 0)
-                temperature   (float) [K]             (default: 298)
-                mfp_air       (float) [m]             (default: 66.4e-9)
-                dyn_vis_air   (float) [kg/m/s]        (default: 1.8e-05)
+                diff_knu        (float) [dimensionless]
+
+            Notes:
+                The dimensionless coagulation kernel is defined as
+                a function of the diffusive knudsen number; for more info,
+                please see the documentation of the respective function:
+                    - particula.util.diffusive_knudsen.diff_knu(**kwargs)
         """
 
-        self.radius = radius
-        self.other_radius = other_radius
-        self.density = density
-        self.other_density = other_density
-        self.charge = charge
-        self.other_charge = other_charge
-        self.temperature = temperature
-        self.mfp_air = mfp_air
-        self.dyn_vis_air = dyn_vis_air
+        self.diff_knu = kwargs.get("diff_knu")
+        self.authors = kwargs.get("authors", "hardsphere")
 
-    def hard_sphere(self):
-
+    def hardsphere_coag(self):
         """ Dimensionless particle--particle coagulation kernel.
         """
 
-        HS_CS = [25.836, 11.211, 3.502, 7.211]
-        diff_knu = diffusive_knudsen(
-            self.radius, self.other_radius,
-            self.density, self.other_density,
-            self.charge, self.other_charge,
-            self.temperature,
-            self.mfp_air,
-            self.dyn_vis_air,
-        )
+        hsa_consts = [25.836, 11.211, 3.502, 7.211]
+        diff_knu = self.diff_knu
 
         upstairs = (
-            (4 * np.pi * difkn**2) +
-            (HS_CS[0] * difkn**3) +
-            ((8 * np.pi)**(1/2) * HS_CS[1] * difkn**4)
+            (4 * np.pi * diff_knu**2) +
+            (hsa_consts[0] * diff_knu**3) +
+            ((8 * np.pi)**(1/2) * hsa_consts[1] * diff_knu**4)
         )
 
         downstairs = (
             1 +
-            (HS_CS[2] * difkn) +
-            (HS_CS[3] * difkn**2) +
-            (HS_CS[1] * difkn**3)
+            (hsa_consts[2] * diff_knu) +
+            (hsa_consts[3] * diff_knu**2) +
+            (hsa_consts[1] * diff_knu**3)
         )
 
         return upstairs / downstairs
+
+    def coag(self):
+        """ Return the dimensionless coagulation kernel.
+        """
+
+        if self.authors == "hardsphere":
+            coag = self.hardsphere_coag()
+        else:
+            raise ValueError(f"{self.authors} not recognized!")
+
+        return coag
