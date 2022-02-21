daa6152699fdfd853b9b7b2a07624084089d283b
ngam <67342040+ngam@users.noreply.github.com>
2022-02-21 18:57:18 -0500

    add scipy, add lognrom pdf + interval

---
+++ particula/aerosol_dynamics/tests/particle_distribution.py
@@ -0,0 +1,47 @@
+""" Constructing a particle distribution
+
+    This module contains the functions to construct a particle distribution
+    from a given set of parameters, like the number of particles, the
+    distribution of the particle sizes, etc
+"""
+
+import numpy as np
+from scipy.stats import lognorm
+
+
+class ParticleDistribution:
+    """ make a class
+    """
+
+    def __init__(self, **kwargs):
+        """ constructing
+        """
+
+        self.mode = kwargs.get("mode", None)
+        self.nbins = kwargs.get("nbins", 1000)
+        self.nparticles = kwargs.get("nparticles", 1e5)
+        self.gsigma = kwargs.get("gsigma", 1.25)
+
+        self.kwargs = kwargs
+
+    def rad(self):
+        """ construct the radius
+        """
+
+        (rad_start, rad_end) = lognorm.interval(
+            alpha=.9999,
+            s=np.log(self.gsigma),
+            scale=self.mode,
+        )
+
+        return np.linspace(rad_start, rad_end, self.nbins)
+
+    def lnd(self):
+        """ discritize the distribution
+        """
+
+        return lognorm.pdf(
+            x=self.rad(), 
+            s=np.log(self.gsigma), 
+            scale=self.mode,
+        )
---
+++ particula/util/lognormal_distribution.py
@@ -0,0 +1,12 @@
+""" make a lognormal distribution
+"""
+
+from scipy.stats import lognorm
+
+def lognormal(**kwargs):
+    """ make a lognormal distribution
+    """
+
+    mode = kwargs.get("mode", None)
+
+    pass
