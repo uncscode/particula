a312ca1007a6f7b4e7514093ee542ba00f68a40a
ngam <67342040+ngam@users.noreply.github.com>
2022-03-07 16:04:13 -0500

    condensation routines: add cbar

---
+++ particula/util/rms_speed.py
@@ -0,0 +1,35 @@
+""" calculating rms speed of molecules
+"""
+
+import numpy as np
+from particula.constants import GAS_CONSTANT, MOLECULAR_WEIGHT_AIR
+from particula.util.input_handling import in_molecular_weight, in_temperature
+
+
+def cbar(**kwargs):
+    """ Returns the mean speed of molecules in an ideal gas.
+
+        Parameters: (either # or $)
+            temperature           (float) [K]      (default: 298.15)
+            molecular_weight      (float) [kg/mol] (default: constants)
+
+        Returns:
+                                  (float) [m/s]
+
+        Using particula.constants:
+            GAS_CONSTANT            (float) [J/mol/K]
+            MOLECULAR_WEIGHT_AIR    (float) [kg/mol]
+
+    """
+
+    temp = kwargs.get("temperature", 298.15)
+    molec_wt = kwargs.get("molecular_weight", MOLECULAR_WEIGHT_AIR)
+
+    temp = in_temperature(temp)
+    molec_wt = in_molecular_weight(molec_wt)
+
+    gas_con = GAS_CONSTANT
+
+    return (
+        (8 * gas_con * temp/(np.pi * molec_wt))**0.5
+    ).to_base_units()
