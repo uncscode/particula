111739f0cdbc6b41990d057477e3618f287ab1b5
ngam <67342040+ngam@users.noreply.github.com>
2022-03-12 17:43:05 -0500

    simplify input_handling.py (#170)
    * simplify input_handling.py
    Fixes #169
    
    * simplify input_handling.py
    Fixes #169

--- particula/util/input_handling.py
+++ particula/util/input_handling.py
@@ -4,137 +4,6 @@
 from particula import u
 
 
-def in_temperature(temp):
-    """ Handles temperature input
-
-        Parameters:
-            temp    (float) [K | dimensionless]
-
-        Returns:
-            temp    (float) [K]
-
-        Notes:
-            * If unit is correct, take to base units in kelvin
-            * Throws ValueError if unit is wrong
-            * Assigning kelvin units to scalar input
-    """
-
-    if isinstance(temp, u.Quantity):
-        if temp.to_base_units().u == u.kelvin:
-            temp = temp.to_base_units()
-        else:
-            raise ValueError(
-                f"\n\t"
-                f"Input {temp} has unsupported units.\n\t"
-                f"Input must have temperature units of\n\t"
-                f"either 'kelvin' or equivalent;\n\t"
-                f"otherwise, if dimensionless, it will\n\t"
-                f"be assigned a kelvin unit.\n"
-            )
-    else:
-        temp = u.Quantity(temp, u.K)
-
-    return temp
-
-
-def in_viscosity(vis):
-    """ Handles viscosity input
-
-        Parameters:
-            vis     (float) [kg/m/s | dimensionless]
-
-        Returns:
-            vis     (float) [kg/m/s]
-
-        Notes:
-            * If unit is correct, take to base units in Pa*s
-            * Throws ValueError if unit is wrong
-            * Assigning Pa*s units to scalar input
-    """
-
-    if isinstance(vis, u.Quantity):
-        if vis.to_base_units().u == u.kg/u.m/u.s:
-            vis = vis.to_base_units()
-        else:
-            raise ValueError(
-                f"\n\t"
-                f"Input {vis} has unsupported units.\n\t"
-                f"Input must have viscosity units of\n\t"
-                f"either 'Pa*s' or equivalent;\n\t"
-                f"otherwise, if dimensionless, it will\n\t"
-                f"be assigned a Pa*s unit.\n"
-            )
-    else:
-        vis = u.Quantity(vis, u.kg/u.m/u.s)
-
-    return vis
-
-
-def in_pressure(pressure):
-    """ Handles pressure input
-
-        Parameters:
-            pressure    (float) [kg/m/s^2 | dimensionless]
-
-        Returns:
-            pressure    (float) [kg/m/s^2]
-
-        Notes:
-            * If unit is correct, take to base units in Pa
-            * Throws ValueError if unit is wrong
-            * Assigning Pa units to scalar input
-    """
-
-    if isinstance(pressure, u.Quantity):
-        if pressure.to_base_units().u == u.kg/u.m/u.s**2:
-            pressure = pressure.to_base_units()
-        else:
-            raise ValueError(
-                f"\n\t"
-                f"Input {pressure} has unsupported units.\n\t"
-                f"Input must have units of 'pascal' or equivalent;\n\t"
-                f"otherwise, if dimensionless, it will\n\t"
-                f"be assigned a Pa unit.\n"
-            )
-    else:
-        pressure = u.Quantity(pressure, u.kg/u.m/u.s**2)
-
-    return pressure
-
-
-def in_molecular_weight(molec_wt):
-    """ Handles molecular weight input
-
-        Parameters:
-            molec_wt    (float) [kg/mol | dimensionless]
-
-        Returns:
-            molec_wt    (float) [kg/mol]
-
-        Notes:
-            * If unit is correct, take to base units in kg/mol
-            * Throws ValueError if unit is wrong
-            * Assigning kg/mol units to scalar input
-    """
-
-    if isinstance(molec_wt, u.Quantity):
-        if molec_wt.to_base_units().u == u.kg/u.mol:
-            molec_wt = molec_wt.to_base_units()
-        else:
-            raise ValueError(
-                f"\n\t"
-                f"Input {molec_wt} has unsupported units.\n\t"
-                f"Input must have molecular weight units of\n\t"
-                f"'kg/mol';\n\t"
-                f"otherwise, if dimensionless, it will.\n\t"
-                f"be assigned a kg/mol unit.\n"
-            )
-    else:
-        molec_wt = u.Quantity(molec_wt, u.kg / u.mol)
-
-    return molec_wt
-
-
 def in_radius(radius):
     """ Handles radius input
 
@@ -171,130 +40,50 @@ def in_radius(radius):
     return radius
 
 
-def in_density(density):
-    """ Handles density input
+def in_handling(value, units: u.Quantity):
+    """ generic function to handle inputs
 
         Parameters:
-            density    (float) [kg/m^3 | dimensionless]
+            value     (float)       [u.Quantity | dimensionless]
+            units     (u.Quantity)
 
         Returns:
-            density    (float) [kg/m^3]
+            value     (float)       [u.Quantity]
 
         Notes:
-            * If unit is correct, take to base units in kg/m^3
+            * If unit is correct, take to base units
             * Throws ValueError if unit is wrong
-            * Assigning kg/m^3 units to scalar input
+            * Assigning default base units to scalar input
     """
 
-    if isinstance(density, u.Quantity):
-        if density.to_base_units().u == u.kg/u.m**3:
-            density = density.to_base_units()
+    if isinstance(value, u.Quantity):
+        if value.to_base_units().u == (1*units).to_base_units().u:
+            value = value.to_base_units()
         else:
             raise ValueError(
                 f"\n\t"
-                f"Input {density} has unsupported units.\n\t"
-                f"Input must have density units of\n\t"
-                f"'kg/m^3';\n\t"
+                f"Input has unsupported units.\n\t"
+                f"Input must have units equivlanet to {units};\n\t"
                 f"otherwise, if dimensionless, it will\n\t"
-                f"be assigned a kg/m^3 unit.\n"
+                f"be assigned {units}.\n"
             )
     else:
-        density = u.Quantity(density, u.kg/u.m**3)
+        value = u.Quantity(value, (1*units).to_base_units().u)
 
-    return density
-
-
-def in_scalar(scalar):
-    """ Handles scalar input
-
-        Parameters:
-            scalar    (float) [dimensionless]
-
-        Returns:
-            scalar    (float) [dimensionless]
-
-        Notes:
-            * If unit is correct, take to base units in dimensionless
-            * Throws ValueError if unit is wrong
-            * Assigning dimensionless units to scalar input
-    """
-
-    if isinstance(scalar, u.Quantity):
-        if scalar.to_base_units().u == u.dimensionless:
-            scalar = scalar.to_base_units()
-        else:
-            raise ValueError(
-                f"\n\t"
-                f"Input {scalar} has unsupported units.\n\t"
-                f"Input must have dimensionless units;\n\""
-            )
-    else:
-        scalar = u.Quantity(scalar, u.dimensionless)
-
-    return scalar
+    return value
 
 
-def in_length(length):
-    """ Handles length input
-
-        Parameters:
-            length    (float) [m | dimensionless]
-
-        Returns:
-            length    (float) [m]
-
-        Notes:
-            * If unit is correct, take to base units in m
-            * Throws ValueError if unit is wrong
-            * Assigning m units to scalar input
-    """
-
-    if isinstance(length, u.Quantity):
-        if length.to_base_units().u == u.m:
-            length = length.to_base_units()
-        else:
-            raise ValueError(
-                f"\n\t"
-                f"Input {length} has unsupported units.\n\t"
-                f"Input must have length units of\n\t"
-                f"'meters';\n\t"
-                f"otherwise, if dimensionless, it will\n\t"
-                f"be assigned a m unit.\n"
-            )
-    else:
-        length = u.Quantity(length, u.m)
-
-    return length
-
-
-def in_volume(volume):
-    """ Handles volume input
-
-        Parameters:
-            volume    (float) [m^3 | dimensionless]
-
-        Returns:
-            volume    (float) [m^3]
-
-        Notes:
-            * If unit is correct, take to base units in m^3
-            * Throws ValueError if unit is wrong
-            * Assigning m^3 units to scalar input
-    """
-
-    if isinstance(volume, u.Quantity):
-        if volume.to_base_units().u == u.m**3:
-            volume = volume.to_base_units()
-        else:
-            raise ValueError(
-                f"\n\t"
-                f"Input {volume} has unsupported units.\n\t"
-                f"Input must have volume units of\n\t"
-                f"'meters^3';\n\t"
-                f"otherwise, if dimensionless, it will\n\t"
-                f"be assigned a m^3 unit.\n"
-            )
-    else:
-        volume = u.Quantity(volume, u.m**3)
-
-    return volume
+# pylint: disable=missing-docstring, multiple-statements
+def in_temperature(temp): return in_handling(temp, u.K)
+def in_viscosity(vis): return in_handling(vis, u.kg/u.m/u.s)
+def in_pressure(pres): return in_handling(pres, u.Pa)
+def in_mass(mass): return in_handling(mass, u.kg)
+def in_volume(vol): return in_handling(vol, u.m**3)
+def in_time(time): return in_handling(time, u.s)
+def in_velocity(vel): return in_handling(vel, u.m/u.s)
+def in_acceleration(acc): return in_handling(acc, u.m/u.s**2)
+def in_molecular_weight(molw): return in_handling(molw, u.kg/u.mol)
+def in_density(density): return in_handling(density, u.kg/u.m**3)
+def in_scalar(scalar): return in_handling(scalar, u.dimensionless)
+def in_length(length): return in_handling(length, u.m)
+def in_area(area): return in_handling(area, u.m**2)
