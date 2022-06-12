0de0af340c8d02e3561af8b19a03e7e0e8dd300a
ngam <67342040+ngam@users.noreply.github.com>
2022-06-12 13:18:51 +0000

    improve lfak8

--- particula/util/lf2013_coagulation/src_lf2013_coagulation.py
+++ particula/util/lf2013_coagulation/src_lf2013_coagulation.py
@@ -5,9 +5,8 @@ import os
 
 import numpy as np
 
-
 # pylint: disable=too-many-arguments, too-many-locals, too-many-branches
-def lf2013_coag_full(
+def lf2013_coag_full( # flake8: noqa: C901
     ion_type="air",
     particle_type="conductive",
     temperature_val=298.15,
