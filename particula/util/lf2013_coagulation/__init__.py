9a5c04aab80b6d562e739ee61c3d6b0a4333e8a2
ngam <67342040+ngam@users.noreply.github.com>
2022-06-12 04:06:12 +0000

    try simplifying

---
+++ particula/util/lf2013_coagulation/__init__.py
@@ -0,0 +1,9 @@
+""" return the function here
+"""
+
+from particula.util.lf2013_coagulation.src_lf2013_coagulation import (
+    lf2013_coag_full)
+
+__all__ = [
+    "lf2013_coag_full",
+]
---
+++ particula/util/lf2013_coagulation/tests/src_lf2013_coagulation_test.py
@@ -0,0 +1,14 @@
+""" test the coag approx utility for lf2013
+"""
+
+# import pytest
+
+from particula.util.lf2013_coagulation import lf2013_coag_full
+
+# from particula import u
+
+
+def test_approx_coag_less():
+    """ testing
+    """
+    assert 1 == 1
