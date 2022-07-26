fix units in simple_solver (#267)

particula/util/simple_solver.py
@@ -15,6 +15,7 @@ import numpy as np
 from scipy.integrate import odeint
 from particula.util.coagulation_rate import CoagulationRate
 from particula import u
+from particula.util.input_handling import in_handling
 
 
 def ode_func(_nums, _, _rads, _coag):
@@ -23,7 +24,7 @@ def ode_func(_nums, _, _rads, _coag):
     coag = CoagulationRate(
         distribution=_nums * u.m**-4,
         radius=_rads * u.m,
-        kernel=_coag * u.m**-4,
+        kernel=_coag * u.m**3/u.s,
     )
 
     return coag.coag_gain().m - coag.coag_loss().m
@@ -50,9 +51,15 @@ class SimpleSolver:
             - kernel: associated coagulation kernel (m**3/s)
             - tspan: desired time span (s)
         """
-        self.nums_init = kwargs.get("distribution", None)
-        self.rads_init = kwargs.get("radius", None)
-        self.coag_kern = kwargs.get("kernel", None)
+        self.nums_init = in_handling(
+            kwargs.get("distribution"), u.m**-4
+        )
+        self.rads_init = in_handling(
+            kwargs.get("radius"), u.m
+        )
+        self.coag_kern = in_handling(
+            kwargs.get("kernel"), u.m**3/u.s
+        )
         self.time_span = kwargs.get("tspan", np.linspace(0, 10, 1000))
         self.kwargs = kwargs
 