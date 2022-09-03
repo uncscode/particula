7e445b0626d0d1f987ba8ad262b1049300634087
ngam <67342040+ngam@users.noreply.github.com>
2022-09-03 15:49:17 +0000

    small edits

--- partycula/party.py
+++ partycula/party.py
@@ -3,31 +3,36 @@
 from collections import Counter
 
 
-class Party:
-    """ a party of partycles """
+class PreGame:
+    """ PreGame """
     def __init__(self, radius):
         self.radius = radius if isinstance(radius, list) else [radius]
 
     def __repr__(self):
         return (
-            f"radius: {list(Counter(self.radius).keys())}\n"
-            f"counts: {list(Counter(self.radius).values())}"
+            f"radius (m): {[f'{i:.2e}' for i in Counter(self.radius).keys()]}\n"
+            f"counts (#): {[f'{i:.2e}' for i in Counter(self.radius).values()]}"
         )
 
     def __add__(self, other):  # self + other
-        return Party(radius=self.radius + other.radius)
+        return PreGame(radius=self.radius + other.radius)
 
     def __radd__(self, other):  # other + self
-        return Party(radius=other.radius + self.radius)
+        return PreGame(radius=other.radius + self.radius)
 
     def __sub__(self, other):  # self - other
         orig = self.radius.copy()
         for i in [j for j in other.radius if j in orig]:
             _ = orig.remove(i) if i in orig else orig
-        return Party(radius=orig)
+        return PreGame(radius=orig)
 
     def __rsub__(self, other):  # other - self
         orig = other.radius.copy()
         for i in [j for j in self.radius if j in orig]:
             _ = orig.remove(i) if i in orig else orig
-        return Party(radius=orig)
+        return PreGame(radius=orig)
+
+
+class Party(PreGame):
+    """ Party """
+    pass
