1ef9a9fcf0f7814b85f6f1871329f76adeb66016
ngam <67342040+ngam@users.noreply.github.com>
2022-10-22 22:18:07 -0400

    some fixes

--- particula/util/condensation_force.py
+++ particula/util/condensation_force.py
@@ -3,37 +3,44 @@
     https://www.nature.com/articles/nature18271
 """
 
-from particula.util.kelvin_correction import kelvin_term
+from particula.util.kelvin_correction import kelvin_term as ktfun
 
 
 def condensation_force(
     vapor_concentraton,
     sat_vapor_concentration,
-    full_particle_activity,
+    particle_activity=None,
+    **kwargs
 ):
     """ calculate the condensation driving force
 
         Equation (9) in https://www.nature.com/articles/nature18271
     """
+    fpa = particle_activity_fun(**kwargs) if particle_activity is None \
+        else particle_activity
+
     return (
         vapor_concentraton -
         sat_vapor_concentration *
-        full_particle_activity
+        fpa
     )
 
 
-def particle_activity(
+def particle_activity_fun(
     mass_fraction,
     activity_coefficient,
-    the_kelvin_term=kelvin_term(),
+    kelvin_term=None,
+    **kwargs
 ):
     """ calculate the particle activity
 
         Equation (9--10) in https://www.nature.com/articles/nature18271
     """
 
+    ktf = ktfun(**kwargs) if kelvin_term is None else kelvin_term
+
     return (
         mass_fraction *
         activity_coefficient *
-        the_kelvin_term
+        ktf
     )
