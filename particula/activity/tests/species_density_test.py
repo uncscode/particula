4504ad67d0d0399a1e52b7ce743333f272350025
Kyle Gorkowski <kkgorkow@gmail.com>
2023-11-18 14:22:59 -0700

    updated density test

--- particula/activity/tests/species_density_test.py
+++ particula/activity/tests/species_density_test.py
@@ -1,4 +1,4 @@
-"""Test the species_density module."""
+"""Test the species_density module if it runs without errors."""
 
 from particula.activity.species_density import organic_density_estimate
 
@@ -10,48 +10,42 @@ def test_organic_density_estimate():
     oxygen2carbon = 0.5
     hydrogen2carbon = 1.0
     nitrogen2carbon = 0.1
-    expected_density = 1.23456789
 
     assert organic_density_estimate(
         molar_mass,
         oxygen2carbon,
         hydrogen2carbon,
-        nitrogen2carbon) == expected_density
+        nitrogen2carbon) > 0.0
 
     # Test case 2: Unknown hydrogen2carbon
     molar_mass = 200.0
     oxygen2carbon = 0.4
-    hydrogen2carbon = -1.0
     nitrogen2carbon = 0.2
-    expected_density = 1.3456789
 
     assert organic_density_estimate(
         molar_mass,
         oxygen2carbon,
-        hydrogen2carbon,
-        nitrogen2carbon) == expected_density
+        nitrogen2carbon) > 0.0
 
     # Test case 3: No nitrogen2carbon
     molar_mass = 150.0
     oxygen2carbon = 0.6
     hydrogen2carbon = 0.8
-    expected_density = 1.123456789
 
     assert organic_density_estimate(
         molar_mass,
         oxygen2carbon,
-        hydrogen2carbon) == expected_density
+        hydrogen2carbon) > 0.0
 
     # Test case 4: Mass ratio conversion
-    molar_mass = 250.0
+    molar_mass = 18/250.0
     oxygen2carbon = 0.3
     hydrogen2carbon = 1.2
     nitrogen2carbon = 0.05
-    expected_density = 1.456789
 
     assert organic_density_estimate(
         molar_mass,
         oxygen2carbon,
         hydrogen2carbon,
         nitrogen2carbon,
-        mass_ratio_convert=True) == expected_density
+        mass_ratio_convert=True) > 0.0
