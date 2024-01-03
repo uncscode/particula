d9476afd6b78830f78f063a044ace165aab92698
Kyle Gorkowski <kkgorkow@gmail.com>
2024-01-03 12:09:50 -0700

    testing out solve_ivp, trying to find negative value injections

--- docs/examples/wall_loss/wall_loss_forward_sim_draft.py
+++ docs/examples/wall_loss/wall_loss_forward_sim_draft.py
@@ -2,13 +2,14 @@
 # flake8: noqa
 
 # %%
+from particula import u
 from particula.util.simple_solver import SimpleSolver
-from particula import particle, rates
+from particula.dynamics import Solver
+from particula.rates import Rates
+from particula import particle
 import numpy as np
 from matplotlib import pyplot as plt
-from particula.util.coagulation_rate import CoagulationRate
 from particula.util.dimensionless_coagulation import full_coag
-from particula.util.input_handling import in_time
 
 # %%
 simple_dic_kwargs = {
@@ -17,26 +18,38 @@ simple_dic_kwargs = {
     "nparticles": 1e6,  # 1e4 #
     "volume": 1e-6,  # per 1e-6 m^3 (or 1 cc)
     "gsigma": 1.5,  # relatively narrow
-    ""
+    "dilution_rate_coefficient": 0.1 * u.hour**-1,
 }
+
+
 # pass the kwargs using ** prefix
 particle_dist2 = particle.Particle(**simple_dic_kwargs)
 
 # inital distribution coag kernel
-coag_kernel = full_coag(radius=particle_dist2.particle_radius)
-time_array = np.arange(0, 1000, 10)
+time_array = np.linspace(0, 1000, 100)
 
-# setup the inital state of the distribution
-problem = {
-    "distribution": particle_dist2.particle_distribution(),
-    "radius": particle_dist2.particle_radius,
-    "kernel": coag_kernel,
-    "tspan": time_array
-}
 
 # call the solver
-solution = SimpleSolver(**problem).solution()
+# solution = SimpleSolver(**problem).solution()
 
+# call dynamics solver
+rates_kwargs = {
+    "particle": particle_dist2,
+    "lazy": False,
+}
+
+solution2 = Solver(
+    time_span=time_array,
+    do_coagulation=True,
+    do_condensation=False,
+    do_nucleation=False,
+    do_dilution=True,
+    do_wall_loss=False,
+    **rates_kwargs
+).solution(method='solve_ivp')
+
+print(solution2.shape)
+#%%
 # plot
 fig, ax = plt.subplots(1, 1, figsize=[9, 6])
 
@@ -46,16 +59,14 @@ ax.semilogx(
     particle_dist2.particle_distribution().m,
     '-b',
     label='Inital')
-ax.semilogx(radius, solution.m[49, :], '--', label='t=50')
-ax.semilogx(radius, solution.m[-1, :], '-r', label='t=end')
+ax.semilogx(radius, solution2.m[49, :], '--', label='t=50')
+ax.semilogx(radius, solution2.m[-1, :], '-r', label='t=end')
 
 ax.legend()
 ax.set_ylabel(f"Number, {particle_dist2.particle_distribution().u}")
 ax.set_xlabel(f"Radius, {particle_dist2.particle_radius.u}")
 ax.grid(True, alpha=0.5)
+plt.show()
 
 
-# %% [markdown]
-# ## summary on stepping
-# As we walked through, using the ODE solver is quite a nice way to get to the answer, without figuring out what time-step you might need.
-#
+# %%
--- particula/dynamics.py
+++ particula/dynamics.py
@@ -1,6 +1,6 @@
 """ statics -> dynamics
 """
-from scipy.integrate import odeint
+from scipy.integrate import odeint, solve_ivp
 
 from particula.rates import Rates
 
@@ -12,9 +12,11 @@ class Solver(Rates):
     def __init__(
         self,
         time_span=None,
-        do_coag=True,
-        do_cond=True,
-        do_nucl=True,
+        do_coagulation=True,
+        do_condensation=True,
+        do_nucleation=True,
+        do_dilution=False,
+        do_wall_loss=False,
         **kwargs
     ):
         """ constructor
@@ -25,9 +27,11 @@ class Solver(Rates):
             raise ValueError("You must provide a time span!")
 
         self.time_span = time_span
-        self.do_coag = do_coag
-        self.do_cond = do_cond
-        self.do_nucl = do_nucl
+        self.do_coagulation = do_coagulation
+        self.do_condensation = do_condensation
+        self.do_nucleation = do_nucleation
+        self.do_dilution = do_dilution
+        self.do_wall_loss = do_wall_loss
 
     def _ode_func(self, _nums, _,):
         """ ode_func
@@ -39,17 +43,37 @@ class Solver(Rates):
         )
 
         return self.sum_rates(
-            coagulation=self.do_coag,
-            condensation=self.do_cond,
-            nucleation=self.do_nucl,
+            coagulation=self.do_coagulation,
+            condensation=self.do_condensation,
+            nucleation=self.do_nucleation,
+            dilution=self.do_dilution,
+            wall_loss=self.do_wall_loss,
         ).m
 
-    def solution(self):
+    def solution(
+            self,
+            method='odeint',
+            **kwargs_ode
+    ):
         """ solve the equation
         """
+        if method == 'odeint':
+            return odeint(
+                func=self._ode_func,
+                y0=self.particle.particle_distribution().m,
+                t=self.time_span,
+                **kwargs_ode
+            )*self.particle.particle_distribution().u
 
-        return odeint(
-            func=self._ode_func,
-            y0=self.particle.particle_distribution().m,
-            t=self.time_span,
-        )*self.particle.particle_distribution().u
+        if method == 'solve_ivp':
+            if 'method' not in kwargs_ode:
+                kwargs_ode['method'] = 'BDF' # Choose an appropriate method
+            return solve_ivp(
+                fun=self._ode_func,
+                t_span=(self.time_span[0], self.time_span[-1]),
+                y0=self.particle.particle_distribution().m,
+                t_eval=self.time_span,
+                **kwargs_ode
+            ).y.T*self.particle.particle_distribution().u
+
+        raise ValueError("Invalid method!")
