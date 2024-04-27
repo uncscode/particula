1ac16559911ede3c202ab883bfe665c753d480d6
Kyle Gorkowski <kkgorkow@gmail.com>
2024-04-27 06:32:34 -0600

    move kelvin to functions

--- particula/next/surface.py
+++ particula/next/surface.py
@@ -17,6 +17,68 @@ from particula.util.convert import (
 from particula.constants import GAS_CONSTANT
 
 
+def kelvin_radius(
+    effective_surface_tension: Union[float, NDArray[np.float_]],
+    effective_density: Union[float, NDArray[np.float_]],
+    molar_mass: Union[float, NDArray[np.float_]],
+    temperature: float,
+) -> Union[float, NDArray[np.float_]]:
+    """
+        Calculate the Kelvin radius which determines the curvature effect on
+        vapor pressure.
+
+        Args:
+        -----
+        - surface_tension (float or NDArray[float]): Surface tension of the
+        mixture [N/m].
+        - molar_mass (float or NDArray[float]): Molar mass of the species
+        [kg/mol].
+        - mass_concentration (float or NDArray[float]): Concentration of the
+        species [kg/m^3].
+        - temperature (float): Temperature of the system [K].
+
+        Returns:
+        --------
+        - float or NDArray[float]: Kelvin radius [m].
+
+        References:
+        -----------
+        - Based on Neil Donahue's approach to the Kelvin equation:
+        r = 2 * surface_tension * molar_mass / (R * T * density)
+        See more: https://en.wikipedia.org/wiki/Kelvin_equation
+        """
+    return (
+        (2 * effective_surface_tension * molar_mass)
+        / (GAS_CONSTANT.m * temperature * effective_density)
+    )
+
+
+def kelvin_term(
+    radius: Union[float, NDArray[np.float_]],
+    kelvin_radius_value: Union[float, NDArray[np.float_]],
+) -> Union[float, NDArray[np.float_]]:
+    """
+    Calculate the Kelvin term, which quantifies the effect of particle
+    curvature on vapor pressure.
+
+    Args:
+    -----
+    - radius (float or NDArray[float]): Radius of the particle [m].
+    - kelvin_radius (float or NDArray[float]): Kelvin radius [m].
+
+    Returns:
+    --------
+    - float or NDArray[float]: The exponential factor adjusting vapor
+    pressure due to curvature.
+
+    References:
+        Based on Neil Donahue's approach to the Kelvin equation:
+        exp(kelvin_radius / particle_radius)
+        See more: https://en.wikipedia.org/wiki/Kelvin_equation
+    """
+    return np.exp(kelvin_radius_value / radius)
+
+
 class SurfaceStrategy(ABC):
     """
     Abstract class for implementing strategies to calculate surface tension and
@@ -65,7 +127,7 @@ class SurfaceStrategy(ABC):
         self,
         molar_mass: Union[float, NDArray[np.float_]],
         mass_concentration: Union[float, NDArray[np.float_]],
-        temperature: float,
+        temperature: float
     ) -> Union[float, NDArray[np.float_]]:
         """
         Calculate the Kelvin radius which determines the curvature effect on
@@ -73,6 +135,8 @@ class SurfaceStrategy(ABC):
 
         Args:
         -----
+        - surface_tension (float or NDArray[float]): Surface tension of the
+        mixture [N/m].
         - molar_mass (float or NDArray[float]): Molar mass of the species
         [kg/mol].
         - mass_concentration (float or NDArray[float]): Concentration of the
@@ -89,11 +153,11 @@ class SurfaceStrategy(ABC):
         r = 2 * surface_tension * molar_mass / (R * T * density)
         See more: https://en.wikipedia.org/wiki/Kelvin_equation
         """
-        return (
-            (2 * self.effective_surface_tension(mass_concentration)
-             * molar_mass)
-            / (GAS_CONSTANT.m * temperature
-               * self.effective_density(mass_concentration))
+        return kelvin_radius(
+            self.effective_surface_tension(mass_concentration),
+            self.effective_density(mass_concentration),
+            molar_mass,
+            temperature
         )
 
     def kelvin_term(
@@ -126,9 +190,10 @@ class SurfaceStrategy(ABC):
             exp(kelvin_radius / particle_radius)
             See more: https://en.wikipedia.org/wiki/Kelvin_equation
         """
-        kelvin_radius = self.kelvin_radius(
-            molar_mass, mass_concentration, temperature)
-        return np.exp(kelvin_radius / radius)
+        return kelvin_term(
+            radius,
+            self.kelvin_radius(molar_mass, mass_concentration, temperature),
+        )
 
 
 # Surface mixing strategies
