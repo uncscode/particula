8d23fe4c36ef0efed4568ba8f426e68d433d714a
Kyle Gorkowski <kkgorkow@gmail.com>
2024-05-03 18:17:04 -0600

    update module doc

--- particula/tests/logging_setup_test.py
+++ particula/tests/logging_setup_test.py
@@ -1,4 +1,9 @@
-"""Tests for the logging setup module."""
+"""Tests for the logging setup module.
+
+This module verifies that the logger configuration is correctly loaded,
+including the correct setup of handlers and formatters as specified in
+the configuration dictionary.
+"""
 
 import logging
 import logging.handlers
