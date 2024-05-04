20f0d19e663ffd82bbea22504e75cd9f864f7a40
Kyle Gorkowski <kkgorkow@gmail.com>
2024-05-03 18:03:14 -0600

    added test

--- particula/tests/logging_setup_test.py
+++ particula/tests/logging_setup_test.py
@@ -14,10 +14,10 @@ def test_setup_configures_logger():
     # Assert
     assert logger.name == expected_logger_name
 
-    logger.info("This is a test message.")
-    logger.debug("This is a debug message.")
-    logger.error("This is an error message.")
-    logger.warning("This is a warning message.")
-    logger.critical("This is a critical message.")
-    logger.exception("This is an exception message.")
+    logger.info("Logger Test: This is a test message.")
+    logger.debug("Logger Test: This is a debug message.")
+    logger.error("Logger Test: This is an error message.")
+    logger.warning("Logger Test: This is a warning message.")
+    logger.critical("Logger Test: This is a critical message.")
+    logger.exception("Logger Test: This is an exception message.")
     assert True  # if no exception is raised, all logging levels are working
