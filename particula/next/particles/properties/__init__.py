7587a8f5ea584f645c5c8ea2b7ad09c0310723a0
Kyle Gorkowski <kkgorkow@gmail.com>
2024-05-11 16:58:13 -0600

    module name space fixing add module suffix

--- particula/next/particles/properties/__init__.py
+++ particula/next/particles/properties/__init__.py
@@ -6,12 +6,12 @@ particula.next.particles.properties.
 # flake8: noqa
 # pyright: basic
 
-from particula.next.particles.properties.aerodynamic_mobility import particle_aerodynamic_mobility
-from particula.next.particles.properties.mean_thermal_speed import mean_thermal_speed
-from particula.next.particles.properties.slip_correction import cunningham_slip_correction
-from particula.next.particles.properties.knudsen_number import calculate_knudsen_number
-from particula.next.particles.properties.friction_factor import friction_factor
-from particula.next.particles.properties import coulomb_enhancement
-from particula.next.particles.properties.diffusive_knudsen import diffusive_knudsen_number
-from particula.next.particles.properties.vapor_correction import vapor_transition_correction
-from particula.next.particles.properties.partial_pressure import partial_pressure_delta
+from particula.next.particles.properties.aerodynamic_mobility_module import particle_aerodynamic_mobility
+from particula.next.particles.properties.mean_thermal_speed_module import mean_thermal_speed
+from particula.next.particles.properties.slip_correction_module import cunningham_slip_correction
+from particula.next.particles.properties.knudsen_number_module import calculate_knudsen_number
+from particula.next.particles.properties.friction_factor_module import friction_factor
+from particula.next.particles.properties import coulomb_enhancement as coulomb_enhancement
+from particula.next.particles.properties.diffusive_knudsen_module import diffusive_knudsen_number
+from particula.next.particles.properties.vapor_correction_module import vapor_transition_correction
+from particula.next.particles.properties.partial_pressure_module import partial_pressure_delta
