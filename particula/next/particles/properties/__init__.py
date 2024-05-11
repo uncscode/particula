6f0126befe560a42cce00e6bc42cb64781dc73ec
Kyle Gorkowski <kkgorkow@gmail.com>
2024-05-11 16:58:45 -0600

    update init

--- particula/next/particles/properties/__init__.py
+++ particula/next/particles/properties/__init__.py
@@ -11,7 +11,7 @@ from particula.next.particles.properties.mean_thermal_speed_module import mean_t
 from particula.next.particles.properties.slip_correction_module import cunningham_slip_correction
 from particula.next.particles.properties.knudsen_number_module import calculate_knudsen_number
 from particula.next.particles.properties.friction_factor_module import friction_factor
-from particula.next.particles.properties import coulomb_enhancement as coulomb_enhancement
+from particula.next.particles.properties import coulomb_enhancement
 from particula.next.particles.properties.diffusive_knudsen_module import diffusive_knudsen_number
 from particula.next.particles.properties.vapor_correction_module import vapor_transition_correction
 from particula.next.particles.properties.partial_pressure_module import partial_pressure_delta
