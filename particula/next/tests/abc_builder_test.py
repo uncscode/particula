29bd1c3496ca59ba3cc32a1fb9ee1de650232240
Kyle Gorkowski <kkgorkow@gmail.com>
2024-05-27 10:16:55 -0600

    fixe name space in abc builder tes

--- particula/next/tests/abc_builder_test.py
+++ particula/next/tests/abc_builder_test.py
@@ -16,42 +16,42 @@ class ConcreteBuilder(BuilderABC):
 
 # Setup the fixture for the testing
 @pytest.fixture
-def builder():
+def builder_fix():
     return ConcreteBuilder(required_parameters=['param1', 'param2'])
 
 
-def test_builder_init(builder):
-    assert builder.required_parameters == [
+def test_builder_init(builder_fix):
+    assert builder_fix.required_parameters == [
         'param1', 'param2'], "Initialization of required parameters failed"
 
 
-def test_check_keys_valid(builder):
+def test_check_keys_valid(builder_fix):
     parameters = {'param1': 10, 'param2': 20, 'param1_units': 'meters'}
     try:
-        builder.check_keys(parameters)
+        builder_fix.check_keys(parameters)
     except ValueError as e:
         pytest.fail(f"Unexpected ValueError raised: {e}")
 
 
-def test_check_keys_missing_required(builder):
+def test_check_keys_missing_required(builder_fix):
     parameters = {'param1': 10}  # missing 'param2'
     with pytest.raises(ValueError) as excinfo:
-        builder.check_keys(parameters)
+        builder_fix.check_keys(parameters)
     assert 'Missing required parameter(s): param2' in str(excinfo.value)
 
 
-def test_check_keys_invalid(builder):
+def test_check_keys_invalid(builder_fix):
     parameters = {'param1': 10, 'param2': 20, 'param3': 30}
     with pytest.raises(ValueError) as excinfo:
-        builder.check_keys(parameters)
+        builder_fix.check_keys(parameters)
     assert "Trying to set an invalid parameter(s)" in str(excinfo.value)
 
 
-def test_pre_build_check_complete(builder):
-    builder.param1 = 10
-    builder.param2 = 20
+def test_pre_build_check_complete(builder_fix):
+    builder_fix.param1 = 10
+    builder_fix.param2 = 20
     try:
-        builder.pre_build_check()
+        builder_fix.pre_build_check()
     except ValueError as e:
         pytest.fail(f"Unexpected ValueError raised: {e}")
 
