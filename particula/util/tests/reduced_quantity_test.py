ff48e8f0be43792c0d2c5947b0d31cc919346d6f
Kyle Gorkowski <kkgorkow@gmail.com>
2024-10-16 13:10:15 -0600

    reduced quantity test update

--- particula/util/tests/reduced_quantity_test.py
+++ particula/util/tests/reduced_quantity_test.py
@@ -3,34 +3,8 @@
 
 import numpy as np
 import pytest
-from particula import u
 from particula.util.reduced_quantity import (
-    reduced_quantity, reduced_value, reduced_self_broadcast)
-
-
-def test_reduced_quantity():
-    """ Test that the reduced quantity is calculated correctly.
-    """
-
-    assert (
-        reduced_quantity(1, 2) ==
-        pytest.approx(2/3)
-    )
-    assert (
-        reduced_quantity(1 * u.kg, 2 * u.kg).magnitude ==
-        pytest.approx(2/3)
-    )
-
-    assert reduced_quantity([1, 2], [2, 3]).shape == (2,)
-
-    assert reduced_quantity(
-        np.array([1, 2]), np.transpose([np.array([2, 3])])
-    ).shape == (2, 2)
-
-    with pytest.raises(TypeError):
-        reduced_quantity(1, 2 * u.kg)
-    with pytest.raises(TypeError):
-        reduced_quantity(1 * u.kg, 2 * u.m)
+    reduced_value, reduced_self_broadcast)
 
 
 def test_reduced_value_scalar():
