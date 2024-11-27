9891b841ae68fdd318f8490ad91aa63e928b9020
Kyle Gorkowski <kkgorkow@gmail.com>
2024-11-27 14:13:15 -0700

    updated the variable names and coefficients

--- particula/particles/properties/collision_radius_module.py
+++ particula/particles/properties/collision_radius_module.py
@@ -1,17 +1,24 @@
-"""Radius of collision module for non-spherical particles."""
+"""Radius of collision module for non-spherical particles. Based on the
+compiled models in Qian et al. (2022).
 
-from typing import Union
+Qian, W., Kronenburg, A., Hui, X., Lin, Y., & Karsch, M. (2022).
+Effects of agglomerate characteristics on their collision kernels in
+the free molecular regime. Journal of Aerosol Science, 159.
+https://doi.org/10.1016/j.jaerosci.2021.10586
+"""
+
+from typing import Union, Tuple
 import numpy as np
 from numpy.typing import NDArray
 
 
 def mulholland_1988(
-    radius_giration: Union[NDArray[np.float64], float]
+    radius_gyration: Union[NDArray[np.float64], float]
 ) -> Union[NDArray[np.float64], float]:
     """Collision radius is equal to the radius of gyration.
 
     Args:
-        radius_giration: Radius of gyration of the particle [m].
+        radius_gyration: Radius of gyration of the particle [m].
 
     Returns:
         (float or NDArray[float]): Collision radius of the particle [m].
@@ -20,18 +27,24 @@ def mulholland_1988(
         Mulholland, G. W., Mountain, R. D., Samson, R. J., & Ernst, M. H.
         (1988). Cluster Size Distribution for Free Molecular Agglomeration.
         Energy and Fuels, 2(4). https://doi.org/10.1021/ef00010a014
+
+    Examples:
+        ``` py title="Example"
+        mulholland_1988(1.5)
+        # 1.5
+        ```
     """
-    return radius_giration
+    return radius_gyration
 
 
 def rogak_flagan_1992(
-    radius_giration: Union[NDArray[np.float64], float],
+    radius_gyration: Union[NDArray[np.float64], float],
     fractal_dimension: Union[NDArray[np.float64], float],
 ) -> Union[NDArray[np.float64], float]:
     """Collision radius with fractal dimension by Rogak and Flagan 1992.
 
     Args:
-        radius_giration: Radius of gyration of the particle [m].
+        radius_gyration: Radius of gyration of the particle [m].
         fractal_dimension: Fractal dimension of the particle
             [dimensionless, df].
 
@@ -43,18 +56,24 @@ def rogak_flagan_1992(
         agglomerates in the transition regime. Journal of Colloid and
         Interface Science, 151(1), 203-224.
         https://doi.org/10.1016/0021-9797(92)90252-H
+
+    Examples:
+        ``` py title="Example"
+        rogak_flagan_1992(1.5, 2.5)
+        # 1.8027756377319946
+        ```
     """
-    return np.sqrt((fractal_dimension + 2) / 3) * radius_giration
+    return np.sqrt((fractal_dimension + 2) / 3) * radius_gyration
 
 
 def zurita_gotor_2002(
-    radius_giration: Union[NDArray[np.float64], float],
+    radius_gyration: Union[NDArray[np.float64], float],
     fractal_prefactor: Union[NDArray[np.float64], float],
 ) -> Union[NDArray[np.float64], float]:
     """Collision radius according to Zurita-Gotor and Rosner (2002).
 
     Args:
-        radius_giration: Radius of gyration of the particle [m].
+        radius_gyration: Radius of gyration of the particle [m].
         fractal_prefactor: Fractal prefactor of the particle
             [dimensionless, k0].
 
@@ -67,59 +86,208 @@ def zurita_gotor_2002(
         aerosol coagulation frequency predictions. Journal of Colloid and
         Interface Science, 255(1).
         https://doi.org/10.1006/jcis.2002.8634
+
+    Examples:
+        ``` py title="Example"
+        zurita_gotor_2002(1.5, 1.2)
+        # 1.568850650368
+        ```
     """
-    return 1.037 * (fractal_prefactor**0.077) * radius_giration
+    return 1.037 * (fractal_prefactor**0.077) * radius_gyration
 
 
-def thajudeen_rc(sigma: float, df: float, n: float, rg: float) -> float:
-    """Collision radius according to Thajudeen et al. (2012)."""
-    alpha1 = 0.253 * df**2 - 1.209 * df + 1.433
-    alpha2 = -0.218 * df**2 + 0.964 * df - 0.180
-    phi = 1 / (alpha1 * np.log(n) + alpha2)
-    rs_i = phi * rg
-    rs_ii = (sigma * (1.203 - 0.4315 / df) / 2) * (4 * rs_i / sigma) ** (
-        0.8806 + 0.3497 / df
+def thajudeen_2012(
+    fractal_dimension: float,
+    number_of_particles: float,
+    radius_gyration: Union[NDArray[np.float64], float],
+    radius_monomer: float,
+) -> Union[NDArray[np.float64], float]:
+    """Collision radius according to Thajudeen et al. (2012).
+
+    Args:
+        fractal_dimension: Fractal dimension of the particle [dimensionless].
+        number_of_particles: Number of particles in the aggregate.
+        radius_gyration: Radius of gyration of the particle [m].
+        radius_monomer: Monomer radius [m].
+
+    Returns:
+        (float): Collision radius of the particle [m].
+
+    References:
+        Thajudeen, T., Gopalakrishnan, R., & Hogan, C. J. (2012). The
+        collision rate of nonspherical particles and aggregates for all
+        diffusive knudsen numbers. Aerosol Science and Technology, 46(11).
+        https://doi.org/10.1080/02786826.2012.701353
+
+    Examples:
+        ``` py title="Example"
+        thajudeen_2012(2.5, 100, 1.5, 0.1)
+        # 0.075
+        ```
+    """
+    alpha1 = 0.253 * fractal_dimension**2 - 1.209 * fractal_dimension + 1.433
+    alpha2 = -0.218 * fractal_dimension**2 + 0.964 * fractal_dimension - 0.180
+    phi = 1 / (alpha1 * np.log(number_of_particles) + alpha2)
+    radius_s_i = phi * radius_gyration
+    radius_s_ii = (
+        radius_monomer * (1.203 - 0.4315 / fractal_dimension) / 2
+    ) * (4 * radius_s_i / radius_monomer) ** (
+        0.8806 + 0.3497 / fractal_dimension
     )
-    return rs_ii / 2
+    return radius_s_ii / 2
 
 
-def model_rg(c1: float, c6: float, rg_tilde: float) -> float:
-    """New model Rc with Rg parameter."""
-    return c1 * rg_tilde + c6
+def qian_2022_rg(
+    radius_gyration: Union[NDArray[np.float64], float],
+    radius_monomer: float,
+    coefficient: Tuple = (0.973, 0.441),
+) -> Union[NDArray[np.float64], float]:
+    """Fitted model using radius of gyration.
+
+    Args:
+        radius_gyration: radius of gyration [m].
+        radius_monomer: monomer radius [m].
+
+    Returns:
+        (float): Collision radius of the particle [m].
+
+    References:
+        Qian, W., Kronenburg, A., Hui, X., Lin, Y., & Karsch, M. (2022).
+        Effects of agglomerate characteristics on their collision kernels in
+        the free molecular regime. Journal of Aerosol Science, 159.
+        https://doi.org/10.1016/j.jaerosci.2021.105868
+
+    Examples:
+        ``` py title="Example"
+        qian_2022_rg(1.5, 0.1)
+        # 0.583
+        ```
+    """
+    return (
+        coefficient[0]
+        * (radius_gyration / radius_monomer)
+        + coefficient[1]
+    ) * radius_monomer
 
 
-def model_rg_df(
-    c1: float, c2: float, c6: float, df: float, rg_tilde: float
-) -> float:
-    """New model Rc with Rg and Df parameters."""
-    return c1 * (df**c2) * rg_tilde + c6
+def qian_2022_rg_df(
+    fractal_dimension: Union[NDArray[np.float64], float],
+    radius_gyration: Union[NDArray[np.float64], float],
+    radius_monomer: float,
+    coefficient: Tuple = (0.882, 0.223, 0.387),
+) -> Union[NDArray[np.float64], float]:
+    """New model Rc with Rg and Df parameters.
+
+    Args:
+        fractal_dimension: Fractal dimension of the particle [-].
+        radius_gyration: Scaled radius of gyration [m].
+        radius_monomer: Monomer radius [m].
+        coefficient: Coefficients for the model
+
+    Returns:
+        (float): Collision radius of the particle [m].
+
+    References:
+        Qian, W., Kronenburg, A., Hui, X., Lin, Y., & Karsch, M. (2022).
+        Effects of agglomerate characteristics on their collision kernels in
+        the free molecular regime. Journal of Aerosol Science, 159.
+        https://doi.org/10.1016/j.jaerosci.2021.105868
+
+    Examples:
+        ``` py title="Example"
+        qian_2022_rg_df(2.5, 1.5, 0.1)
+        # 0.583
+        ```
+    """
+    return (
+        coefficient[0]
+        * (fractal_dimension**coefficient[1])
+        * (radius_gyration / radius_monomer)
+        + coefficient[2]
+    ) * radius_monomer
 
 
-def model_rg_df_k0(
-    c1: float,
-    c2: float,
-    c3: float,
-    c4: float,
-    c6: float,
-    df: float,
-    k0: float,
-    rg_tilde: float,
-) -> float:
-    """New model Rc with Rg, Df, and k0 parameters."""
-    return c1 * (df**c2) * (k0**c3) * rg_tilde + c4 * k0 + c6
+def qian_2022_rg_df_k0(
+    fractal_dimension: float,
+    fractal_prefactor: float,
+    radius_gyration: Union[NDArray[np.float64], float],
+    radius_monomer: float,
+    coefficient: Tuple = (0.777, 0.479, 0.000970, 0.267, -0.0790),
+) -> Union[NDArray[np.float64], float]:
+    """New model Rc with Rg, Df, and k0 parameters.
+
+    Args:
+        fractal_dimension: Fractal dimension of the particle [-].
+        fractal_prefactor: Fractal prefactor of the particle [-].
+        radius_gyration: radius of gyration [m].
+        radius_monomer: monomer radius [m].
+        coefficient: Coefficients for the model
+
+    Returns:
+        (float): Collision radius of the particle [m].
+
+    References:
+        Qian, W., Kronenburg, A., Hui, X., Lin, Y., & Karsch, M. (2022).
+        Effects of agglomerate characteristics on their collision kernels in
+        the free molecular regime. Journal of Aerosol Science, 159.
+        https://doi.org/10.1016/j.jaerosci.2021.105868
+
+    Examples:
+        ``` py title="Example"
+        qian_2022_rg_df_k0(2.5, 1.2, 1.5, 0.1)
+        # 0.583
+        ```
+    """
+    return (
+        coefficient[0]
+        * (fractal_dimension**coefficient[1])
+        * (fractal_prefactor**coefficient[2])
+        * (radius_gyration / radius_monomer)
+        + coefficient[3] * fractal_prefactor
+        + coefficient[4]
+    ) * radius_monomer
 
 
-def model_rg_df_k0_a13(
-    c1: float,
-    c2: float,
-    c3: float,
-    c4: float,
-    c5: float,
-    c6: float,
-    df: float,
-    k0: float,
-    a13: float,
-    rg_tilde: float,
-) -> float:
-    """New model Rc with Rg, Df, k0, and A13 parameters."""
-    return c1 * (df**c2) * (k0**c3) * rg_tilde + c4 * k0 + c5 * a13 + c6
+# pylint: disable=too-many-arguments
+def qian_2022_rg_df_k0_a13(
+    fractal_dimension: float,
+    fractal_prefactor: float,
+    shape_anisotropy: float,
+    radius_gyration: Union[NDArray[np.float64], float],
+    radius_monomer: float,
+    coefficient: Tuple = (0.876, 0.363, -0.105, 0.421, -0.0360, -0.227),
+) -> Union[NDArray[np.float64], float]:
+    """New model Rc with Rg, Df, k0, and A13 parameters.
+
+    Args:
+        fractal_dimension: Fractal dimension of the particle [-].
+        fractal_prefactor: Fractal prefactor of the particle [-].
+        shape_anisotropy: Parameter A13 for the model [-].
+        radius_gyration: Radius of gyration [m].
+        radius_monomer: Monomer radius [m].
+        coefficient: Coefficients for the model
+
+    Returns:
+        (float): Collision radius of the particle [m].
+
+    References:
+        Qian, W., Kronenburg, A., Hui, X., Lin, Y., & Karsch, M. (2022).
+        Effects of agglomerate characteristics on their collision kernels in
+        the free molecular regime. Journal of Aerosol Science, 159.
+        https://doi.org/10.1016/j.jaerosci.2021.105868
+
+    Examples:
+        ``` py title="Example"
+        qian_2022_rg_df_k0_a13(2.5, 1.2, 0.5, 1.5, 0.1)
+        # 0.583
+        ```
+    """
+    return (
+        coefficient[0]
+        * (fractal_dimension**coefficient[1])
+        * (fractal_prefactor**coefficient[2])
+        * (radius_gyration / radius_monomer)
+        + coefficient[3] * fractal_prefactor
+        + coefficient[4] * shape_anisotropy
+        + coefficient[5]
+    ) * radius_monomer
