cdf889cf5d4cc4d5a52d3724aa488a10d8175fb2
Kyle Gorkowski <kkgorkow@gmail.com>
2024-11-30 18:27:15 -0700

    Document Builder ABC Fixes #475

--- particula/abc_builder.py
+++ particula/abc_builder.py
@@ -24,22 +24,22 @@ class BuilderABC(ABC):
     """Abstract base class for builders with common methods to check keys and
     set parameters from a dictionary.
 
-    Attributes:
-        required_parameters: List of required parameters for the builder.
+    Args:
+        - required_parameters: List of required parameters for the builder.
 
     Methods:
-        check_keys (parameters): Check if the keys you want to set are
-        present in the parameters dictionary.
-        set_parameters (parameters): Set parameters from a dictionary including
-        optional suffix for units as '_units'.
-        pre_build_check(): Check if all required attribute parameters are set
-        before building.
-        build (abstract): Build and return the strategy object.
+        - check_keys (parameters): Check if the keys you want to set are
+        - present in the parameters dictionary.
+        - set_parameters (parameters): Set parameters from a dictionary
+            including optional suffix for units as '_units'.
+        - pre_build_check(): Check if all required attribute parameters are set
+            before building.
+        - build (abstract): Build and return the strategy object.
 
     Raises:
-        ValueError: If any required key is missing during check_keys or
-        pre_build_check, or if trying to set an invalid parameter.
-        Warning: If using default units for any parameter.
+        - ValueError: If any required key is missing during check_keys or
+            pre_build_check, or if trying to set an invalid parameter.
+        - Warning: If using default units for any parameter.
 
     References:
         This module also defines mixin classes for the Builder classes to set
@@ -54,11 +54,11 @@ class BuilderABC(ABC):
         """Check if the keys are present and valid.
 
         Args:
-            parameters: The parameters dictionary to check.
+            - parameters: The parameters dictionary to check.
 
         Raises:
-            ValueError: If any required key is missing or if trying to set an
-            invalid parameter.
+            - ValueError: If any required key is missing or if trying to set an
+                invalid parameter.
         """
 
         # Check if all required keys are present
@@ -92,14 +92,14 @@ class BuilderABC(ABC):
         units as '_units'.
 
         Args:
-            parameters: The parameters dictionary to set.
+            - parameters : The parameters dictionary to set.
 
         Returns:
-            self: The builder object with the set parameters.
+            - The builder object with the set parameters.
 
         Raises:
-            ValueError: If any required key is missing.
-            Warning: If using default units for any parameter.
+            - ValueError : If any required key is missing.
+            - Warning : If using default units for any parameter.
         """
         self.check_keys(parameters)
         for key in self.required_parameters:
@@ -118,8 +118,8 @@ class BuilderABC(ABC):
     def pre_build_check(self):
         """Check if all required attribute parameters are set before building.
 
-        Raises:
-            ValueError: If any required parameter is missing.
+            Raises:
+                - ValueError : If any required parameter is missing.
         """
         if missing := [
             p for p in self.required_parameters if getattr(self, p) is None
@@ -135,5 +135,5 @@ class BuilderABC(ABC):
         """Build and return the strategy object with the set parameters.
 
         Returns:
-            strategy: The built strategy object.
+            - strategy : The built strategy object.
         """
