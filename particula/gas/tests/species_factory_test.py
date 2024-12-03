929f87dcab84f552e7ff85a43a1d215bf6c272ba
Kyle Gorkowski <kkgorkow@gmail.com>
2024-12-02 18:58:36 -0700

    format test docs

--- particula/gas/tests/species_factory_test.py
+++ particula/gas/tests/species_factory_test.py
@@ -2,16 +2,24 @@
 
 import unittest
 from particula.gas.species_factories import GasSpeciesFactory
-from particula.gas.species_builders import GasSpeciesBuilder, PresetGasSpeciesBuilder
+from particula.gas.species_builders import (
+    GasSpeciesBuilder,
+    PresetGasSpeciesBuilder,
+)
 from particula.gas.species import GasSpecies
-from particula.gas.vapor_pressure_strategies import ConstantVaporPressureStrategy
+from particula.gas.vapor_pressure_strategies import (
+    ConstantVaporPressureStrategy,
+)
 
 
 class TestGasSpeciesFactory(unittest.TestCase):
+    """Test the GasSpeciesFactory class."""
     def setUp(self):
+        """Set up the test case."""
         self.factory = GasSpeciesFactory()
 
     def test_get_builders(self):
+        """Test getting the builders."""
         builders = self.factory.get_builders()
         self.assertIsInstance(builders["gas_species"], GasSpeciesBuilder)
         self.assertIsInstance(
@@ -19,6 +27,7 @@ class TestGasSpeciesFactory(unittest.TestCase):
         )
 
     def test_get_strategy_gas_species(self):
+        """Test getting a gas species strategy."""
         parameters = {
             "name": "Oxygen",
             "molar_mass": 0.032,
@@ -30,6 +39,7 @@ class TestGasSpeciesFactory(unittest.TestCase):
         self.assertIsInstance(gas_species, GasSpecies)
 
     def test_get_strategy_preset_gas_species(self):
+        """Test getting a preset gas species strategy."""
         parameters = {}
         preset_gas_species = self.factory.get_strategy(
             "preset_gas_species", parameters
@@ -37,6 +47,6 @@ class TestGasSpeciesFactory(unittest.TestCase):
         self.assertIsInstance(preset_gas_species, GasSpecies)
 
     def test_get_strategy_invalid(self):
+        """Test getting an invalid strategy."""
         with self.assertRaises(ValueError):
             self.factory.get_strategy("invalid_type", {})
-
