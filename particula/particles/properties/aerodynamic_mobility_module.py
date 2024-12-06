8d3fd6bb52f75e9a61a81925e688441f8448bf57
Kyle Gorkowski <kkgorkow@gmail.com>
2024-12-05 18:16:49 -0700

    added example to docs

--- particula/particles/properties/aerodynamic_mobility_module.py
+++ particula/particles/properties/aerodynamic_mobility_module.py
@@ -12,20 +12,29 @@ def particle_aerodynamic_mobility(
     dynamic_viscosity: float,
 ) -> Union[float, NDArray[np.float64]]:
     """
-    Calculate the aerodynamic mobility of a particle, defined as the ratio
-    of the slip correction factor to the product of the dynamic viscosity of
-    the fluid, the particle radius, and a slip correction constant derived.
+    Calculate the aerodynamic mobility of a particle.
 
-    This mobility quantifies the ease with which a particle can move through
-    a fluid.
+    This is defined as the ratio of the slip correction factor to the product
+    of the dynamic viscosity of the fluid, the particle radius, and a slip
+    correction constant derived. This mobility quantifies the ease with which
+    a particle can move through a fluid.
 
-    Arguments:
-        radius : The radius of the particle (m).
-        slip_correction_factor : The slip correction factor for the particle
-            in the fluid (dimensionless).
-        dynamic_viscosity : The dynamic viscosity of the fluid (Pa.s).
+    Args:
+        - radius : The radius of the particle (m).
+        - slip_correction_factor : The slip correction factor for the particle
+             in the fluid (dimensionless).
+        - dynamic_viscosity : The dynamic viscosity of the fluid (Pa.s).
 
     Returns:
         The particle aerodynamic mobility (m^2/s).
+
+    Example:
+        ``` py title="Example"
+        aerodynamic_mobility = particle_aerodynamic_mobility(
+            radius=0.00005,
+            slip_correction_factor=1.1,
+            dynamic_viscosity=0.0000181,
+        )
+        ```
     """
     return slip_correction_factor / (6 * np.pi * dynamic_viscosity * radius)
