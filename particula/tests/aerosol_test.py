9159e6f229f5ed235609ca703886de4c24d5ac68
Kyle Gorkowski <kkgorkow@gmail.com>
2024-12-21 09:40:38 -0700

    Testing for Aerosol Class Fixes #487

--- particula/tests/aerosol_test.py
+++ particula/tests/aerosol_test.py
@@ -1,85 +1,102 @@
 """Tests for the Aerosol class.
+"""
 
-Wait for particles to be implemented before running these tests."""
-
-# to handle pytest fixture call error
-# https://docs.pytest.org/en/stable/deprecations.html#calling-fixtures-directly
-# pylint: disable=redefined-outer-name
-# pylint: disable=R0801
-
-# import pytest
-# import numpy as np
-# from particula.aerosol import Aerosol
-# from particula.gas.atmosphere import AtmosphereBuilder
-# from particula.gas.species import GasSpeciesBuilder, GasSpecies
-# from particula.gas.vapor_pressure import ConstantVaporPressureStrategy
-# from particula.particles.representation import Particle, particle_strategy_factory
-# from particula.particles.activity import MassIdealActivity
-# from particula.particles.surface import surface_strategy_factory
-
-# activity_strategy = MassIdealActivity()
-# surface_strategy = surface_strategy_factory()
+import unittest
+import numpy as np
+from particula.particles import PresetParticleRadiusBuilder
+from particula.gas import (
+    AtmosphereBuilder,
+    VaporPressureFactory,
+    GasSpeciesBuilder,
+)
+from particula.aerosol import Aerosol
+from particula.gas.species import GasSpecies
+from particula.particles.representation import ParticleRepresentation
 
 
-# @pytest.fixture
-# def sample_gas():
-#     """Fixture for creating a Gas instance for testing."""
-#     vapor_pressure_strategy = ConstantVaporPressureStrategy(
-#         vapor_pressure=np.array([101325, 101325]))
-#     names = np.array(["Oxygen", "Nitrogen"])
-#     molar_masses = np.array([0.032, 0.028])  # kg/mol
-#     condensables = np.array([False, False])
-#     concentrations = np.array([1.2, 0.8])  # kg/m^3
+class TestAerosol(unittest.TestCase):
+    """Test class for the Aerosol class."""
 
-#     gas_species = (GasSpeciesBuilder()
-#                    .name(names)
-#                    .molar_mass(molar_masses)
-#                    .vapor_pressure_strategy(vapor_pressure_strategy)
-#                    .condensable(condensables)
-#                    .concentration(concentrations)
-#                    .build())
+    def setUp(self):
+        """Set up the test case with an real Atmosphere
+            and ParticleRepresentation.
+        """
+        # Glycerol gas
+        molar_mass_glycerol = 92.09382e-3  # kg/mol
+        parameters_clausius = {
+            "latent_heat": 71.5 * molar_mass_glycerol,
+            "latent_heat_units": "kJ/kg",
+            "temperature_initial": 125.5,
+            "temperature_initial_units": "degC",
+            "pressure_initial": 1,
+            "pressure_initial_units": "mmHg",
+        }
+        vapor_pressure_strategy = VaporPressureFactory().get_strategy(
+            "clausius_clapeyron", parameters_clausius
+        )
 
-#     return (
-#         AtmosphereBuilder()
-#         .temperature(298.15)
-#         .total_pressure(101325)
-#         .add_species(gas_species)
-#         .build()
-#     )
+        sat_concentration = vapor_pressure_strategy.saturation_concentration(
+            molar_mass_glycerol, 298.15
+        )
 
+        sat_factor = 0.5  # 50% of saturation concentration
+        glycerol_gas = (
+            GasSpeciesBuilder()
+            .set_molar_mass(molar_mass_glycerol, "kg/mol")
+            .set_vapor_pressure_strategy(vapor_pressure_strategy)
+            .set_concentration(sat_concentration * sat_factor, "kg/m^3")
+            .set_name("Glycerol")
+            .set_condensable(True)
+            .build()
+        )
 
-# @pytest.fixture
-# def sample_particles():
-#     """Fixture for creating a Particle instance for testing."""
-#     strategy = particle_strategy_factory('mass_based_moving_bin')
-#     return Particle(
-#         strategy,
-#         activity_strategy,
-#         surface_strategy,
-#         np.array([100, 200, 300], dtype=np.float64),
-#         np.float64([2.5]),
-#         np.array([10, 20, 30], dtype=np.float64))
+        self.atmosphere = (
+            AtmosphereBuilder()
+            .add_species(glycerol_gas)
+            .set_temperature(25, temperature_units="degC")
+            .set_pressure(1, pressure_units="atm")
+            .build()
+        )
 
+        # Glycerol particle distribution
+        self.particle = (
+            PresetParticleRadiusBuilder()
+            .set_mode(np.array([100]), "nm")
+            .set_geometric_standard_deviation(np.array([1.5]))
+            .set_number_concentration(np.array([1e4]), "1/cm^3")
+            .set_density(1.26, "g/cm^3")
+            .build()
+        )
 
-# @pytest.fixture
-# def aerosol_with_fixtures(sample_gas, sample_particles):
-#     """Fixture for creating an Aerosol instance with fixtures."""
-#     return Aerosol(gas=sample_gas, particles=sample_particles)
+        self.aerosol = Aerosol(
+            atmosphere=self.atmosphere, particles=self.particle
+        )
 
+    def test_str(self):
+        """Test the __str__ method of the Aerosol class."""
+        result = str(self.aerosol)
+        self.assertIn("Gas mixture", result)
+        self.assertIn("Particle Representation", result)
 
-# def test_add_particle(aerosol_with_fixtures, sample_particles):
-#     """Test adding a Particle instance."""
-#     aerosol_with_fixtures.add_particle(sample_particles)
-#     assert len(aerosol_with_fixtures.particles) == 2
+    def test_iterate_gas(self):
+        """Test the iterate_gas method of the Aerosol class."""
+        gas_iterator = self.aerosol.iterate_gas()
+        self.assertIsInstance(next(gas_iterator), GasSpecies)
 
+    def test_iterate_particle(self):
+        """Test the iterate_particle method of the Aerosol class."""
+        particle_iterator = self.aerosol.iterate_particle()
+        self.assertIsInstance(next(particle_iterator), ParticleRepresentation)
 
-# def test_iterate_gases(aerosol_with_fixtures):
-#     """Test iterating over Gas instances."""
-#     for gas_species in aerosol_with_fixtures.iterate_gas():
-#         assert isinstance(gas_species, GasSpecies)
+    def test_replace_atmosphere(self):
+        """Test the replace_atmosphere method of the Aerosol class."""
+        new_atmosphere = self.atmosphere
+        self.aerosol.replace_atmosphere(new_atmosphere)
+        self.assertEqual(self.aerosol.atmosphere, new_atmosphere)
 
-
-# def test_iterate_particles(aerosol_with_fixtures):
-#     """Test iterating over Particle instances."""
-#     for particle in aerosol_with_fixtures.iterate_particle():
-#         assert isinstance(particle, Particle)
+    def test_add_particle(self):
+        """Test the add_particle method of the Aerosol class."""
+        new_particle = self.particle
+        initial_count = len(self.aerosol.particles)
+        self.aerosol.add_particle(new_particle)
+        self.assertEqual(len(self.aerosol.particles), initial_count + 1)
