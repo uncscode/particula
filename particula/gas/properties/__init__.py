af0b2a1c8f25d9c2483e4f8ca6ba4f0d532f8097
Kyle Gorkowski <kkgorkow@gmail.com>
2025-01-19 12:28:08 -0700

    clean gas.properties init

--- particula/gas/properties/__init__.py
+++ particula/gas/properties/__init__.py
@@ -38,6 +38,3 @@ from particula.gas.properties.integral_scale_module import (
 from particula.gas.properties.fluid_rms_velocity import (
     get_fluid_rms_velocity
 )
-from particula.gas.properties.fluid_rms_velocity import (
-    get_fluid_rms_velocity
-)
