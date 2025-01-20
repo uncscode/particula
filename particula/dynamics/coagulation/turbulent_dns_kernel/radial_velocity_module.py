9d9f5c2fd89b59557cae628b9c3bf8986332b9c2
Kyle Gorkowski <kkgorkow@gmail.com>
2025-01-20 08:12:11 -0700

    tests for radial velocity

--- particula/dynamics/coagulation/turbulent_dns_kernel/radial_velocity_module.py
+++ particula/dynamics/coagulation/turbulent_dns_kernel/radial_velocity_module.py
@@ -1,3 +1,6 @@
+"""
+Radial relative velocity calculation module.
+"""
 from typing import Union
 import numpy as np
 from numpy.typing import NDArray
@@ -5,13 +8,16 @@ from scipy.special import erf
 
 from particula.util.validate_inputs import validate_inputs
 from particula.util.constants import STANDARD_GRAVITY
-from particula.util.self_broadcast import get_pairwise_diff_matrix
+from particula.util.self_broadcast import (
+    get_pairwise_diff_matrix,
+    get_pairwise_sum_matrix,
+)
+
 
 @validate_inputs(
     {
         "velocity_dispersion": "positive",
-        "particle_inertia_time_1": "positive",
-        "particle_inertia_time_2": "positive",
+        "particle_inertia_time": "positive",
     }
 )
 def get_radial_relative_velocity_dz2002(
@@ -64,29 +70,31 @@ def get_radial_relative_velocity_dz2002(
 @validate_inputs(
     {
         "velocity_dispersion": "positive",
-        "particle_inertia_time_1": "positive",
-        "particle_inertia_time_2": "positive",
+        "particle_inertia_time": "positive",
     }
 )
 def get_radial_relative_velocity_ao2008(
     velocity_dispersion: Union[float, NDArray[np.float64]],
-    particle_inertia_time_1: Union[float, NDArray[np.float64]],
-    particle_inertia_time_2: Union[float, NDArray[np.float64]],
-    gravitational_acceleration: float = STANDARD_GRAVITY,
+    particle_inertia_time: Union[float, NDArray[np.float64]],
 ) -> Union[float, NDArray[np.float64]]:
     """
-    Compute the radial relative velocity based on the Ayala et al. (2008) formulation.
+    Compute the radial relative velocity based on the Ayala et al. (2008)
+    formulation.
 
     The relative velocity is given by:
 
         ⟨ |w_r| ⟩ = sqrt(2 / π) * sqrt(σ² + (π/8) * (τ_p1 + τ_p2)² * |g|²)
+
+    - σ : Turbulence velocity dispersion [m/s]
+    - τ_p1, τ_p2 : Inertia timescale of particles 1 and 2 [s]
+    - g : Gravitational acceleration [m/s²]
+
 
     Arguments:
     ----------
         - velocity_dispersion : Turbulence velocity dispersion (σ) [m/s].
         - particle_inertia_time_1 : Inertia timescale of particle 1 (τ_p1) [s].
         - particle_inertia_time_2 : Inertia timescale of particle 2 (τ_p2) [s].
-        - gravitational_acceleration : Gravitational acceleration (default: 9.81 m/s²).
 
     Returns:
     --------
@@ -94,9 +102,12 @@ def get_radial_relative_velocity_ao2008(
 
     References:
     -----------
-    - Ayala, O., Rosa, B., & Wang, L. P. (2008). New Journal of Physics, 10.
+    - Ayala, O., Rosa, B., & Wang, L. P. (2008). Effects of turbulence on
+        the geometric collision rate of sedimenting droplets. Part 2.
+        Theory and parameterization. New Journal of Physics, 10.
+        https://doi.org/10.1088/1367-2630/10/7/075016
     """
-    tau_sum = particle_inertia_time_1 + particle_inertia_time_2
-    gravity_term = (np.pi / 8) * tau_sum**2 * gravitational_acceleration**2
+    tau_sum = get_pairwise_sum_matrix(particle_inertia_time)
+    gravity_term = (np.pi / 8) * tau_sum**2 * STANDARD_GRAVITY**2
 
     return np.sqrt(2 / np.pi) * np.sqrt(velocity_dispersion**2 + gravity_term)
