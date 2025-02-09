fix: Correct calculation of relative velocity variance in the function

particula/dynamics/coagulation/turbulent_dns_kernel/sigma_relative_velocity_ao2008.py
@@ -124,10 +124,7 @@ def get_relative_velocity_variance(
         eulerian_integral_length,
         vel_corr_terms,
     )
-    rms_velocity_matrix = (
-        rms_velocity[:, np.newaxis] + rms_velocity[np.newaxis, :]
-    )
-    return rms_velocity_matrix**2 - 2 * cross_correlation
+    return rms_velocity[:, np.newaxis]**2 + rms_velocity[np.newaxis, :]**2 - 2 * cross_correlation
 
 
 @validate_inputs(