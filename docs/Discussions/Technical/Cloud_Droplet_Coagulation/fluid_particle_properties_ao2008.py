4ce519896c4c6947737e72225c9c1d9a9187ac4c
Kyle Gorkowski <kkgorkow@gmail.com>
2025-02-10 16:51:14 -0700

    chore: Clean up and document fluid particle properties script

--- docs/Discussions/Technical/Cloud_Droplet_Coagulation/fluid_particle_properties_ao2008.py
+++ docs/Discussions/Technical/Cloud_Droplet_Coagulation/fluid_particle_properties_ao2008.py
@@ -1,104 +1,26 @@
-# %%
-from typing import Union
-import numpy as np
-from numpy.typing import NDArray
+# %% [markdown]
 
-import matplotlib.pyplot as plt
-from particula.dynamics.coagulation.turbulent_dns_kernel.radial_velocity_module import (
-    get_radial_relative_velocity_ao2008,
-)
-from particula.dynamics.coagulation.turbulent_dns_kernel.sigma_relative_velocity_ao2008 import (
-    get_relative_velocity_variance,
-    _compute_rms_fluctuation_velocity,
-    VelocityCorrelationTerms,
-)
-from particula.particles.properties.inertia_time import (
-    get_particle_inertia_time,
-)
-from particula.particles.properties.settling_velocity import (
-    get_particle_settling_velocity_via_inertia,
-    get_particle_settling_velocity_with_drag,
-)
+"""
+# Fluid and Particle Properties for Cloud Droplet Coagulation
+
+This notebook checks the `Particula` caculated values against the values from the paper "Aerosol Science and Technology" by Ayala and Rosa (2008).
+
+Reference: Table 2 and Table 3
+
+Ayala, O., Rosa, B., Wang, L. P., & Grabowski, W. W. (2008). Effects of turbulence on the geometric collision rate of sedimenting droplets. Part 1. Results from direct numerical simulation. New Journal of Physics, 10. https://doi.org/10.1088/1367-2630/10/7/075015
+
+"""
+
+
+# %%
+import numpy as np
+
 from particula.particles import properties
 from particula.gas import properties as gas_properties
-from particula.gas.properties.kinematic_viscosity import (
-    get_kinematic_viscosity,
-)
-from particula.gas.properties.dynamic_viscosity import get_dynamic_viscosity
-from particula.gas.properties.fluid_rms_velocity import get_fluid_rms_velocity
-from particula.gas.properties.normalize_accel_variance import (
-    get_normalized_accel_variance_ao2008,
-)
-from particula.gas.properties.kolmogorov_module import get_kolmogorov_time
-from particula.gas.properties.taylor_microscale_module import (
-    get_taylor_microscale,
-    get_lagrangian_taylor_microscale_time,
-)
-from particula.gas.properties.integral_scale_module import (
-    get_eulerian_integral_length,
-    get_lagrangian_integral_time,
-)
+
 from particula.util.constants import STANDARD_GRAVITY
 from particula.util.converting.units import convert_units
 
-from particula.dynamics.coagulation.turbulent_dns_kernel.velocity_correlation_terms_ao2008 import (
-    compute_b1,
-    compute_b2,
-    compute_c1,
-    compute_c2,
-    compute_d1,
-    compute_d2,
-    compute_e1,
-    compute_e2,
-    compute_z,
-    compute_beta,
-)
-
-
-# %% DNS values
-
-
-# Figure 12: Comparison of the predicted and simulated mean-square horizontal
-# particle velocities for droplets falling in a turbulent ﬂow of Rλ = 72.41 and
-# turbulent_dissipation = 400 cm2 s−3
-
-# droplet radius (a2, microns) vs rms_velocity (cm2/s2)
-
-# dns_10cm2/s3: 6 rows, 2 columns (X, Y)
-dns_10cm2_s3 = np.array(
-    [
-        [9.938118812, 26.66666667],
-        [20.02475248, 26.41975309],
-        [30.04950495, 26.41975309],
-        [40.01237624, 24.69135802],
-        [50.16089109, 22.71604938],
-        [60.06188119, 18.51851852],
-    ]
-)
-
-# dns_100_cm2/s3: 6 rows, 2 columns (X, Y)
-dns_100_cm2_s3 = np.array(
-    [
-        [9.938118812, 84.44444444],
-        [20.02475248, 80.98765432],
-        [29.98762376, 77.03703704],
-        [39.95049505, 71.11111111],
-        [49.97524752, 59.25925926],
-        [60.06188119, 44.19753086],
-    ]
-)
-
-# dns_400_cm2/s3: 6 rows, 2 columns (X, Y)
-dns_400_cm2_s3 = np.array(
-    [
-        [9.876237624, 166.9135802],
-        [20.08663366, 163.9506173],
-        [30.11138614, 150.1234568],
-        [40.07425743, 129.1358025],
-        [50.03712871, 100.4938272],
-        [60.06188119, 69.62962963],
-    ]
-)
 
 # %% Model equations
 # Define Particle Radii and Parameters
@@ -115,9 +37,9 @@ reynolds_lambda = 72.41  # Example value
 
 
 # Calculate dynamic and kinematic viscosity
-dynamic_viscosity = get_dynamic_viscosity(temperature)
-kinematic_viscosity = get_kinematic_viscosity(dynamic_viscosity, fluid_density)
-kolmogorov_time = get_kolmogorov_time(
+dynamic_viscosity = gas_properties.get_dynamic_viscosity(temperature)
+kinematic_viscosity = gas_properties.get_kinematic_viscosity(dynamic_viscosity, fluid_density)
+kolmogorov_time = gas_properties.get_kolmogorov_time(
     kinematic_viscosity=kinematic_viscosity,
     turbulent_dissipation=turbulent_dissipation,
 )
@@ -133,7 +55,7 @@ knudsen_number = properties.calculate_knudsen_number(
 )
 slip_correction_factor = properties.cunningham_slip_correction(knudsen_number)
 # iterative terminal settling velocity
-iterative_settling_velocity = get_particle_settling_velocity_with_drag(
+iterative_settling_velocity = properties.get_particle_settling_velocity_with_drag(
     particle_radius=particle_radius,
     particle_density=particle_density,
     fluid_density=fluid_density,
@@ -149,26 +71,22 @@ settling_velocity = properties.particle_settling_velocity(
 )
 relative_velocity = iterative_settling_velocity - air_velocity
 # Calculate Particle Inertia Time
-particle_inertia_time = get_particle_inertia_time(
+particle_inertia_time = properties.get_particle_inertia_time(
     particle_radius=particle_radius,
     particle_density=particle_density,
     fluid_density=fluid_density,
     kinematic_viscosity=kinematic_viscosity,
 )
 
-print(f"v: {kinematic_viscosity}")
-
-
 re_p = 2 * particle_radius * relative_velocity / kinematic_viscosity
 f_re_p = 1 + 0.15 * re_p**0.687
 
-manual_re_p = np.array([0.015, 0.116, 0.378, 0.851, 1.566, 2.537])
-manual_t_p = np.array([0.0013, 0.0052, 0.0118, 0.0209, 0.0327, 0.0471])
-manual_f_re_p = np.array([1.008, 1.034, 1.077, 1.134, 1.204, 1.284])
-v_p = particle_inertia_time * STANDARD_GRAVITY / manual_f_re_p
+ao2008_re_p = np.array([0.015, 0.116, 0.378, 0.851, 1.566, 2.537])
+ao2008_t_p = np.array([0.0013, 0.0052, 0.0118, 0.0209, 0.0327, 0.0471])
+ao2008_f_re_p = np.array([1.008, 1.034, 1.077, 1.134, 1.204, 1.284])
 
 # calculate relative velocity from re_p
-manual_velocity = manual_re_p * kinematic_viscosity / (2 * particle_radius)
+ao2008_velocity = ao2008_re_p * kinematic_viscosity / (2 * particle_radius)
 
 
 particle_settling_velocity = (
@@ -182,25 +100,40 @@ particle_settling_velocity = (
     )
 )
 
-particle_settling_velocity_via_manual = (
-    particle_inertia_time * STANDARD_GRAVITY / manual_f_re_p
-)
-
+# print paper values
+print("Paper Values From Table 2")
+print("Radius (µm) | tp | Settling Velocity (cm/s)| Re_p | f(Re_p)")
+print("-" * 50)
+for radius, tp, settling_velocity, re, f_re in zip(
+    particle_radius,
+    ao2008_t_p,
+    ao2008_velocity * 100,
+    ao2008_re_p,
+    ao2008_f_re_p,
+):
+    print(
+        f"{radius * 1e6:.1f} \t | {tp:.4f} | {settling_velocity:.2f} \t\t | {re:.3f} | {f_re:.3f}"
+    )
 
 # print settling velocity in a table format
+print("Particula Computed Values")
 print("Radius (µm) | tp | Settling Velocity (cm/s)| Re_p | f(Re_p)")
 print("-" * 50)
 for radius, tp, settling_velocity, re, f_re in zip(
     particle_radius,
     particle_inertia_time,
     particle_settling_velocity,
-    # particle_settling_velocity_via_manual,
     re_p,
     f_re_p,
 ):
     print(
         f"{radius * 1e6:.1f} \t | {tp:.4f} | {settling_velocity * 100:.2f} \t\t | {re:.3f} | {f_re:.3f}"
     )
+
+
+# %% Compare Table 3 from paper with computed values
+# Characteristic scales of cloud droplets.
+
 
 # get stokes number table
 length_kolmogorov = gas_properties.get_kolmogorov_length(
@@ -253,8 +186,47 @@ stokes_number_400 = properties.get_stokes_number(
 stokes_velocity_400 = particle_settling_velocity / velocity_kolmogorov_400
 
 
+# from paper
+
+st_ao2008 = np.array(
+    [
+        [0.010, 0.032, 0.063],
+        [0.040, 0.127, 0.253],
+        [0.090, 0.285, 0.570],
+        [0.160, 0.507, 1.014],
+        [0.250, 0.792, 1.585],
+        [0.361, 1.141, 2.282],
+    ]
+)
+sv_ao2008 = np.array(
+    [
+        [1.113, 0.626, 0.442],
+        [4.343, 2.442, 1.727],
+        [9.385, 5.278, 3.732],
+        [15.841, 8.908, 6.299],
+        [23.316, 13.111, 9.271],
+        [31.478, 17.701, 12.516],
+    ]
+)
+
+# print paper values
+print("Paper Values From Table 3")
+print("-" * 80)
+print("Radius (µm) | St (10 cm^2/s^3) | St (100 cm^2/s^3) | St (400 cm^2/s^3)")
+print("-" * 80)
+for radius, st_10, st_100, st_400 in zip(
+    particle_radius,
+    st_ao2008[:, 0],
+    st_ao2008[:, 1],
+    st_ao2008[:, 2],
+):
+    print(
+        f"{radius * 1e6:.1f} \t | {st_10:.4f} \t\t | {st_100:.4f} \t\t | {st_400:.4f}"
+    )
+
 # print stokes number in a table format
 
+print("Particula Computed Values")
 print("Radius (µm) | St (10 cm^2/s^3) | St (100 cm^2/s^3) | St (400 cm^2/s^3)")
 print("-" * 80)
 for radius, st_10, st_100, st_400 in zip(
@@ -267,17 +239,33 @@ for radius, st_10, st_100, st_400 in zip(
         f"{radius * 1e6:.1f} \t | {st_10:.4f} \t\t | {st_100:.4f} \t\t | {st_400:.4f}"
     )
 
+
+# print ao2008 values
+print("Paper Values From Table 3")
+print("-" * 80)
+print("Radius (µm) | Sv (10 cm^2/s^3) | Sv (100 cm^2/s^3) | Sv (400 cm^2/s^3)")
+print("-" * 80)
+for radius, sv_10, sv_100, sv_400 in zip(
+    particle_radius,
+    sv_ao2008[:, 0],
+    sv_ao2008[:, 1],
+    sv_ao2008[:, 2],
+):
+    print(
+        f"{radius * 1e6:.1f} \t | {sv_10:.4f} \t\t | {sv_100:.4f} \t\t | {sv_400:.4f}"
+    )
+
 # print stokes velocity in a table format
 print(
-    "Radius (µm)  | St (10 cm^2/s^3) | St (100 cm^2/s^3) | St (400 cm^2/s^3)"
+    "Radius (µm)  | Sv (10 cm^2/s^3) | Sv (100 cm^2/s^3) | Sv (400 cm^2/s^3)"
 )
 print("-" * 80)
-for radius, st_10, st_100, st_400 in zip(
+for radius, stv_10, stv_100, stv_400 in zip(
     particle_radius,
     stokes_velocity_10,
     stokes_velocity_100,
     stokes_velocity_400,
 ):
     print(
-        f"{radius * 1e6:.1f} \t | {st_10:.4f} \t\t | {st_100:.4f} \t\t | {st_400:.4f}"
+        f"{radius * 1e6:.1f} \t | {stv_10:.4f} \t\t | {stv_100:.4f} \t\t | {stv_400:.4f}"
     )
