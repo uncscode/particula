d3b388d9686395db2aef84bacfc5688685039056
Kyle Gorkowski <kkgorkow@gmail.com>
2025-06-14 13:31:43 -0600

    test: convert quick start script to pytest function with assertion

--- particula/integration_tests/quick_start_test.py
+++ particula/integration_tests/quick_start_test.py
@@ -1,31 +1,36 @@
 import numpy as np
 import particula as par
 
-organic = par.gas.GasSpecies(
-    name="H2O",
-    molar_mass=180e-3,
-    vapor_pressure_strategy=par.gas.ConstantVaporPressureStrategy(1e2, "Pa"),
-    partitioning=True,
-    concentration=1e-4,
-)
+def test_quick_start_runs():
+    organic = par.gas.GasSpecies(
+        name="H2O",
+        molar_mass=180e-3,
+        vapor_pressure_strategy=par.gas.ConstantVaporPressureStrategy(1e2, "Pa"),
+        partitioning=True,
+        concentration=1e-4,
+    )
 
-atm = par.gas.Atmosphere(
-    temperature=298.15,
-    total_pressure=101325,
-    partitioning_species=organic,
-)
+    atm = par.gas.Atmosphere(
+        temperature=298.15,
+        total_pressure=101325,
+        partitioning_species=organic,
+    )
 
-particle = (
-    par.particles.PresetParticleRadiusBuilder()
-    .set_mode(np.array([100e-9]), "m")
-    .set_geometric_standard_deviation(np.array([1.2]))
-    .set_number_concentration(np.array([1e8]), "1/m^3")
-    .set_density(1e3, "kg/m^3")
-    .build()
-)
+    particle = (
+        par.particles.PresetParticleRadiusBuilder()
+        .set_mode(np.array([100e-9]), "m")
+        .set_geometric_standard_deviation(np.array([1.2]))
+        .set_number_concentration(np.array([1e8]), "1/m^3")
+        .set_density(1e3, "kg/m^3")
+        .build()
+    )
 
-aerosol = par.Aerosol(atmosphere=atm, particles=particle)
-process = par.dynamics.MassCondensation(
-    par.dynamics.CondensationIsothermal(0.018)
-)
-print(process.execute(aerosol, 10.0))
+    aerosol = par.Aerosol(atmosphere=atm, particles=particle)
+    process = par.dynamics.MassCondensation(
+        par.dynamics.CondensationIsothermal(0.018)
+    )
+
+    result = process.execute(aerosol, 10.0)
+
+    # Basic sanity check: execution returns an Aerosol instance
+    assert isinstance(result, par.Aerosol)
