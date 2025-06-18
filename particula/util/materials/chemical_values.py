2692ff8ff742bbb046b715fe7d178e073e3ee23b
Kyle Gorkowski <kkgorkow@gmail.com>
2025-06-18 16:01:33 -0600

    test different values

--- particula/util/materials/chemical_values.py
+++ particula/util/materials/chemical_values.py
@@ -19,11 +19,11 @@ print(f"Surface tension of liquid aluminum at {T} K: {value:.5f} N/m")
 print("Temperature range:", gamma.Tmin, "to", gamma.Tmax)
 
 # %% silica mineral
-cas_silica = CAS_from_any("Si")
+cas_silica = CAS_from_any("Si")  # or "Silica, quartz" for crystalline form
 gamma_silica = SurfaceTension(CASRN=cas_silica)
 # Compute surface tension at a given temperature, e.g., 1000 K
 print("Temperature range:", gamma_silica.Tmin, "to", gamma_silica.Tmax)
-T_silica = 1500  # K
+T_silica = 2500  # K
 value_silica = gamma_silica.T_dependent_property(T_silica)  # in N/m
 print(f"Surface tension of silica at {T_silica} K: {value_silica:.5f} N/m")
 
