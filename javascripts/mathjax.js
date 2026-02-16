b5cd3aeb3e85d5822f6a7606ede6b9a59aa11e97
Gorkowski <kkgorkow@gmail.com>
2026-02-16 19:17:35 +0000

    Deploying to gh-pages from @ uncscode/particula@effd3e2db69c60b05e6fcaf1d6e118fbd3b91a05 🚀

--- javascripts/mathjax.js
+++ javascripts/mathjax.js
@@ -1,5 +1,25 @@
 window.MathJax = {
   tex: {
-    inlineMath: {'[+]': [['$', '$']]}
-  }
+    inlineMath: [
+      ["\\(", "\\)"],
+      ["$", "$"],
+    ],
+    displayMath: [
+      ["\\[", "\\]"],
+      ["$$", "$$"],
+    ],
+    processEscapes: true,
+    processEnvironments: true,
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex|jp-RenderedHTMLCommon|jp-RenderedMarkdown",
+  },
 };
+
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache();
+  MathJax.typesetClear();
+  MathJax.texReset();
+  MathJax.typesetPromise();
+});
