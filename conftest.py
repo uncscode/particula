31673b63362cf17ae7e3e31c63eb8e3ff6c11f10
Kyle Gorkowski <Gorkowski@users.noreply.github.com>
2026-06-06 07:55:09 -0600

    update

--- conftest.py
+++ conftest.py
@@ -8,23 +8,37 @@ from __future__ import annotations
 
 import warnings
 
-warnings.filterwarnings(
-    "ignore",
-    message="The NumPy module was reloaded.*",
-    category=UserWarning,
-)
-warnings.filterwarnings(
-    "ignore",
-    message="GasSpecies is deprecated.*",
-    category=DeprecationWarning,
-)
 
-
-def pytest_runtest_setup(item) -> None:
-    """Ensure deprecation warnings are filtered even under -Werror."""
+def _filter_expected_warnings() -> None:
+    """Keep known third-party and compatibility warnings out of CI errors."""
+    warnings.filterwarnings(
+        "ignore",
+        message="The NumPy module was reloaded.*",
+        category=UserWarning,
+        append=False,
+    )
     warnings.filterwarnings(
         "ignore",
         message="GasSpecies is deprecated.*",
         category=DeprecationWarning,
         append=False,
     )
+    warnings.filterwarnings(
+        "ignore",
+        message="Due to '_pack_', the 'APICLaunchParamRecord' Structure.*",
+        category=DeprecationWarning,
+        append=False,
+    )
+
+
+_filter_expected_warnings()
+
+
+def pytest_configure() -> None:
+    """Reapply filters after pytest processes command-line warnings."""
+    _filter_expected_warnings()
+
+
+def pytest_runtest_setup(item) -> None:
+    """Ensure deprecation warnings are filtered even under -Werror."""
+    _filter_expected_warnings()
