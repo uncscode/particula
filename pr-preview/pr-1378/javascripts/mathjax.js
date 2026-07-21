29247acd6ac86f7ad5011952ebf6d3b2b120d4c2
Gorkowski <Gorkowski@users.noreply.github.com>
2026-07-21 11:57:45 +0000

    Deploy preview for PR 1378 🛫

---
+++ pr-preview/pr-1378/javascripts/mathjax.js
@@ -0,0 +1,25 @@
+window.MathJax = {
+  tex: {
+    inlineMath: [
+      ["\\(", "\\)"],
+      ["$", "$"],
+    ],
+    displayMath: [
+      ["\\[", "\\]"],
+      ["$$", "$$"],
+    ],
+    processEscapes: true,
+    processEnvironments: true,
+  },
+  options: {
+    ignoreHtmlClass: ".*|",
+    processHtmlClass: "arithmatex|jp-RenderedHTMLCommon|jp-RenderedMarkdown",
+  },
+};
+
+document$.subscribe(() => {
+  MathJax.startup.output.clearCache();
+  MathJax.typesetClear();
+  MathJax.texReset();
+  MathJax.typesetPromise();
+});
